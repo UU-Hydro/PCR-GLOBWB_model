050f9badbb617eb4af1ddf6d4ff3d78a21a1145b
Edwin Sutanudjaja <sutanudjaja@edwin1.geo.uu.nl>
2014-10-10 16:56:04 +0200

    Add extra evaporation and abstraction.

--- model/routing.py
+++ model/routing.py
@@ -676,7 +676,7 @@ class Routing(object):
                 allocation  = pcr.cover(pcr.areatotal(extraVolAllocSurfaceWaterAbstract, landSurface.allocSegments)/landSurface.segmentArea, 0.0)
             
                 vos.waterBalanceCheck([pcr.ifthen(self.landmask,abstraction)],\
-                                      [pcr.ifthen(allocation)],\
+                                      [pcr.ifthen(self.landmask, allocation)],\
                                       [pcr.scalar(0.0)],\
                                       [pcr.scalar(0.0)],\
                                       'extra surface water abstraction - allocation per zone/segment (PS: Error here may be caused by rounding error.)' ,\
@@ -687,7 +687,7 @@ class Routing(object):
                 allocation  = pcr.cover(pcr.areatotal(landSurface.allocSurfaceWaterAbstract*self.cellArea, landSurface.allocSegments)/landSurface.segmentArea, 0.0)
             
                 vos.waterBalanceCheck([pcr.ifthen(self.landmask,abstraction)],\
-                                      [pcr.ifthen(allocation)],\
+                                      [pcr.ifthen(self.landmask, allocation)],\
                                       [pcr.scalar(0.0)],\
                                       [pcr.scalar(0.0)],\
                                       'surface water abstraction (after extra water) - allocation per zone/segment (PS: Error here may be caused by rounding error.)' ,\
