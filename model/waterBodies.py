302ebabb171c3d3a5f6b4b2ad19b10c9968ba478
Edwin Sutanudjaja <sutanudjaja@edwin1.geo.uu.nl>
2014-10-10 12:03:09 +0200

    Finishing simplifiedKinematicWave.

--- model/waterBodies.py
+++ model/waterBodies.py
@@ -354,8 +354,7 @@ class WaterBodies(object):
         self.waterBodyOutflow = pcr.min(self.waterBodyStorage * factor,\
                                         self.waterBodyOutflow)                    # unit: m3/day
         # use round values 
-        self.waterBodyOutflow = pcr.rounddown(self.waterBodyOutflow/10.)*10.      # unit: m3/day
-        #
+        self.waterBodyOutflow = pcr.rounddown(self.waterBodyOutflow/100.)*100.    # unit: m3/day
         
         # outflow in m3 per sec
         waterBodyOutflowInM3PerSec = self.waterBodyOutflow / vos.secondsPerDay()  # unit: m3/s
