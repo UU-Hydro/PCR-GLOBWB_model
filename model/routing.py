bc3a0b193572d0886ffe14700ef9be62cfdcd8d9
Edwin Sutanudjaja <Edwin Sutanudjaja>
2014-10-14 05:09:30 +0200

    Fixing simplified kinematic wave.

--- model/routing.py
+++ model/routing.py
@@ -864,9 +864,6 @@ class Routing(object):
         #
         ##########################################################################################################################
 
-        # return waterBodyStorage to channelStorage  
-        self.channelStorage = self.return_water_body_storage_to_channel(self.channelStorage)
-
         # calculate the statistics of long and short term flow values
         self.calculate_statistics(groundwater)
         
