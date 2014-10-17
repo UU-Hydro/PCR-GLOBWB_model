b22d34878f4b3895a4c857ced416f82fc1717b30
Edwin Sutanudjaja <sutanudjaja@edwin1.geo.uu.nl>
2014-10-17 15:03:27 +0200

    Fixing interception storage in Sahara.

--- model/variable_list.py
+++ model/variable_list.py
@@ -89,7 +89,7 @@ netcdf_monthly_total_unit[pcrglobwb_variable_name] = None
 netcdf_yearly_total_unit[pcrglobwb_variable_name]  = None
 netcdf_long_name[pcrglobwb_variable_name]  = None
 description[pcrglobwb_variable_name]       = None
-comment[pcrglobwb_variable_name]           = 'Not including water bodies.'
+comment[pcrglobwb_variable_name]           = None
 latex_symbol[pcrglobwb_variable_name]      = None
 
 # snowCoverSWE 
