71b47412246eac2bcd8630c6d0ba32149c9464e2
Edwin Sutanudjaja <sutanudjaja@edwin1.geo.uu.nl>
2014-10-29 18:30:49 +0100

    Add pumping capacity.

--- model/groundwater.py
+++ model/groundwater.py
@@ -147,8 +147,6 @@ class Groundwater(object):
             region_ids = vos.readPCRmapClone(\
                          iniItems.extraOptionsforProjectWithIWMI['regionIds'],
                          self.cloneMap,self.tmpDir,self.inputDir)
-            pcr.report(region_ids,"region_ids.map"); os.system("aguila region_ids.map")
-            
             self.region_ids = pcr.nominal(region_ids)
             self.region_ids = pcr.ifthen(self.landmask, self.region_ids)
             
