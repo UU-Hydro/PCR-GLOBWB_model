7db8d1b885ff536c9696e1dd9e1c6524fc95ba7c
Edwin Sutanudjaja <sutanudjaja@edwin1.geo.uu.nl>
2014-10-31 16:59:08 +0100

    Selecting and fixing ini files for the release 2.0.3.

--- model/landCover.py
+++ model/landCover.py
@@ -483,7 +483,7 @@ class LandCover(object):
         self.totalPotET = pcr.ifthen(self.landmask,\
                                      self.cropKC * meteo.referencePotET)
 
-        pcr.report(self.totalPotET, "totalPotET.map"); os.system("aguila totalPotET.map")
+        pcr.report(self.cropKC * meteo.referencePotET, "totalPotET.map"); os.system("aguila totalPotET.map")
 
         # calculate potential bare soil evaporation and transpiration (unit: m/day)
         self.potBareSoilEvap  = pcr.ifthen(self.landmask,\
