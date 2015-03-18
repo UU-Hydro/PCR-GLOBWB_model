57a0c9b04bf4ce3eee84843020ce72a06ce38c7e
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-03-18 13:37:07 +0100

    First test with irrigation partitioning.

--- model/reporting.py
+++ model/reporting.py
@@ -353,7 +353,8 @@ class Reporting(object):
         # - including: interception, snow, soil and non fossil groundwater 
         self.totalActiveStorageThickness = pcr.ifthen(\
                                            self._model.routing.landmask, \
-                                           self._model.routing.channelStorage / self._model.routing.cellArea + \
+                                           pcr.max(0.0, self._model.routing.channelStorage) /\
+                                           self._model.routing.cellArea + \
                                            self._model.landSurface.totalSto + \
                                            self._model.groundwater.storGroundwater)
 
