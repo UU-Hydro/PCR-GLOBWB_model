a4a4d25eb22fc2ade11ab364fbcddba2006a556e
Edwin Sutanudjaja <Edwin Sutanudjaja>
2015-03-22 20:24:50 +0100

    Try to improve the partitioning of groundwater and surface water source.

--- model/landCover.py
+++ model/landCover.py
@@ -1086,9 +1086,9 @@ class LandCover(object):
 
             logger.debug('Total groundwater abstraction is limited by regional annual pumping capacity.')
 
-            # estimate of total groundwater abstraction (m3) from the last 365 days:
+            # estimate of total groundwater abstraction (m3) from the last 365 days, with the tolerance of 75 days (2.5 months):
             annualGroundwaterAbstraction = groundwater.avgAbstraction * routing.cellArea *\
-                                           pcr.min(365., routing.timestepsToAvgDischarge)
+                                           pcr.min(365. - 75., routing.timestepsToAvgDischarge)
             # at regional scale
             regionalAnnualGroundwaterAbstraction = pcr.areatotal(pcr.cover(annualGroundwaterAbstraction, 0.0), groundwater_pumping_region_ids)
                                                                  
