b7c97aab859fed9164c47f8a1ef9240bc8ddae80
Edwin Sutanudjaja <Edwin Sutanudjaja>
2015-03-25 08:17:13 +0100

    Try to increase demand in HPA.

--- model/landCover.py
+++ model/landCover.py
@@ -926,8 +926,8 @@ class LandCover(object):
             # idea on 25 march - also compensating infiltration losses 
             #                  - openWaterEvap should tackle evaporationDefiict
             #
-            if self.numberOfLayers == 2: self.irrGrossDemand = pcr.min(self.irrGrossDemand, evaporationDeficit + parameters.kSatUpp)
-            if self.numberOfLayers == 3: self.irrGrossDemand = pcr.min(self.irrGrossDemand, evaporationDeficit + parameters.kSatUpp000005)
+            if self.numberOfLayers == 2: self.irrGrossDemand = pcr.ifthenelse(evaporationDeficit > 0, pcr.min(self.irrGrossDemand, evaporationDeficit + parameters.kSatUpp      ), 0.0)
+            if self.numberOfLayers == 2: self.irrGrossDemand = pcr.ifthenelse(evaporationDeficit > 0, pcr.min(self.irrGrossDemand, evaporationDeficit + parameters.kSatUpp000005), 0.0)
             #
             #~ # - assume that smart farmers do not irrigate higher than infiltration capacities - THIS SHOULD NOT BE IMPLEMENTED IF WE ALLOW OPENWATEREVAP FROM NON-PADDY FIELDS
             #~ if self.numberOfLayers == 2: self.irrGrossDemand = pcr.min(self.irrGrossDemand, parameters.kSatUpp)
