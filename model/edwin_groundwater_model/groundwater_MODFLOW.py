01476fb9601babd2c4486b26b61fd4a228040cc7
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-05-24 16:18:26 +0200

    Continue testing (steady state).

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -193,7 +193,7 @@ class GroundwaterModflow(object):
         
         # reducing noise
         bottom_of_bank_storage = pcr.max(bottom_of_bank_storage,\
-                                 pcr.windowaverage(bottom_of_bank_storage, 0.5))
+                                 pcr.windowaverage(bottom_of_bank_storage, 0.25))
 
         # TODO: We may want to improve this concept - by incorporating the following 
         # - smooth bottom_elevation
@@ -303,7 +303,7 @@ class GroundwaterModflow(object):
         MXITER = 100                # maximum number of outer iterations
         ITERI  = 30                 # number of inner iterations
         NPCOND = 1                  # 1 - Modified Incomplete Cholesky, 2 - Polynomial matrix conditioning method;
-        HCLOSE = 0.001              # HCLOSE (unit: m) # 1.0 is working
+        HCLOSE = 0.005              # HCLOSE (unit: m) # 1.0 is working
         RCLOSE = 10.* 400.*400.     # RCLOSE (unit: m3) ; Deltares uses 100 m3 for their 25 m modflow model  
         RELAX  = 1.00               # relaxation parameter used with NPCOND = 1
         NBPOL  = 2                  # indicates whether the estimate of the upper bound on the maximum eigenvalue is 2.0 (but we don ot use it, since NPCOND = 1) 
