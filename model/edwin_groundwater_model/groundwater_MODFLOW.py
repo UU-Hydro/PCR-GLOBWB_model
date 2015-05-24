6e197afcae1b1201ee06d548ca57405e52953c0b
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-05-24 09:29:41 +0200

    First test (steady state).

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -163,9 +163,8 @@ class GroundwaterModflow(object):
         self.pcg_MXITER = 1000               # maximum number of outer iterations
         self.pcg_ITERI  = 250                # number of inner iterations
         self.pcg_NPCOND = 1                  # 1 - Modified Incomplete Cholesky, 2 - Polynomial matrix conditioning method;
-        self.pcg_HCLOSE = 0.001              # HCLOSE (unit: m)
-        self.pcg_RCLOSE = 10.* 10000.*10000. # RCLOSE (unit: m3) ; Deltares uses 10 m3 for their 250 m model (working) 
-        #~ self.pcg_RCLOSE = 10.* 1000.*1000. # RCLOSE (unit: m3) ; Deltares uses 10 m3 for their 250 m model 
+        self.pcg_HCLOSE = 0.005              # HCLOSE (unit: m)
+        self.pcg_RCLOSE = 1000.* 1000.*1000. # RCLOSE (unit: m3) ; Deltares uses 10 m3 for their 25 m model (working) 
 
         self.pcg_RELAX  = 0.98               # relaxation parameter used with NPCOND = 1
         self.pcg_NBPOL  = 2                  # indicates whether the estimate of the upper bound on the maximum eigenvalue is 2.0 (but we don ot use it, since NPCOND = 1) 
