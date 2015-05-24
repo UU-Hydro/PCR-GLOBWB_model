4e264381838f949602f3a3490b4659890f8d830d
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-05-24 16:58:04 +0200

    Continue testing (steady state).

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -185,9 +185,12 @@ class GroundwaterModflow(object):
         # bottom_elevation > flood_plain elevation - influence zone
         bottom_of_bank_storage = self.dem_floodplain - influence_zone_depth
         
-        #~ # bottom_elevation > river bed
-        #~ bottom_of_bank_storage = pcr.max(self.dem_riverbed, bottom_of_bank_storage)
+        # bottom_elevation > river bed
+        bottom_of_bank_storage = pcr.max(self.dem_riverbed, bottom_of_bank_storage)
         
+        # bottom_elevation > its downstream value
+        bottom_of_bank_storage = pcr.max(bottom_of_bank_storage, pcr.downstream(self.lddMap, bottom_of_bank_storage))
+
         # bottom_elevation >= 0.0 (must be higher than sea level)
         bottom_of_bank_storage = pcr.max(0.0, bottom_of_bank_storage)
         
