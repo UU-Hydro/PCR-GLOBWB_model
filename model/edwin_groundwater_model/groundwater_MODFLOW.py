41034ec4f304f479747ab5cb5c043685783e5e2c
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-05-24 10:57:48 +0200

    First test (steady state).

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -165,19 +165,6 @@ class GroundwaterModflow(object):
         
         # TODO: defining/incorporating anisotrophy values
         
-        # default parameter values for the PCG solver:
-        self.pcg_MXITER = 100                # maximum number of outer iterations
-        self.pcg_ITERI  = 30                 # number of inner iterations
-        self.pcg_NPCOND = 1                  # 1 - Modified Incomplete Cholesky, 2 - Polynomial matrix conditioning method;
-        self.pcg_HCLOSE = 1.0                # HCLOSE (unit: m)
-        self.pcg_RCLOSE = 100.* 400.*400.    # RCLOSE (unit: m3) ; Deltares uses 10 m3 for their 25 m modflow model  
-        self.pcg_RELAX  = 0.98               # relaxation parameter used with NPCOND = 1
-        self.pcg_NBPOL  = 2                  # indicates whether the estimate of the upper bound on the maximum eigenvalue is 2.0 (but we don ot use it, since NPCOND = 1) 
-        self.pcg_DAMP   = 1                  # no damping (DAMP introduced in MODFLOW 2000)
-        #
-        # set PCG solver:
-        self.pcr_modflow.setPCG(self.pcg_MXITER, self.pcg_ITERI, self.pcg_NPCOND, self.pcg_HCLOSE, self.pcg_RCLOSE, self.pcg_RELAX, self.pcg_NBPOL, self.pcg_DAMP)
-
         # default parameter values for the DIS solver:
         self.dis_ITMUNI = 4     # indicates the time unit (0: undefined, 1: seconds, 2: minutes, 3: hours, 4: days, 5: years)
         self.dis_LENUNI = 2     # indicates the length unit (0: undefined, 1: feet, 2: meters, 3: centimeters)
@@ -313,6 +300,17 @@ class GroundwaterModflow(object):
 
         # using dem_average as the initial groundwater head value 
         self.pcr_modflow.setInitialHead(self.dem_average, 1)
+        
+        # set PCG solver:
+        MXITER = 100                # maximum number of outer iterations
+        ITERI  = 30                 # number of inner iterations
+        NPCOND = 1                  # 1 - Modified Incomplete Cholesky, 2 - Polynomial matrix conditioning method;
+        HCLOSE = 1.0                # HCLOSE (unit: m)
+        RCLOSE = 1000.* 400.*400.   # RCLOSE (unit: m3) ; Deltares uses 10 m3 for their 25 m modflow model  
+        RELAX  = 1.00               # relaxation parameter used with NPCOND = 1
+        NBPOL  = 2                  # indicates whether the estimate of the upper bound on the maximum eigenvalue is 2.0 (but we don ot use it, since NPCOND = 1) 
+        DAMP   = 1                  # no damping (DAMP introduced in MODFLOW 2000)
+        self.pcr_modflow.setPCG(MXITER, ITERI, NPCOND, HCLOSE, RCLOSE, RELAX, NBPOL, DAMP)
         
         # set parameter values for the DIS package 
         self.pcr_modflow.setDISParameter(self.dis_ITMUNI, self.dis_LENUNI, self.dis_PERLEN, self.dis_NSTP, self.dis_TSMULT, 1)  
