6faff286736804549485b98e613d077fce694a75
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-05-26 20:07:27 +0200

    Test two layer groundwater model.

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -215,7 +215,7 @@ class GroundwaterModflow(object):
     def estimate_bottom_of_bank_storage(self):
 
         # influence zone depth (m)
-        influence_zone_depth = 0.50
+        influence_zone_depth = 5.0
         
         # bottom_elevation > flood_plain elevation - influence zone
         bottom_of_bank_storage = self.dem_floodplain - influence_zone_depth
