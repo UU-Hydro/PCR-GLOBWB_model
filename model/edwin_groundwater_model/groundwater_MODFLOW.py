50601bc0c32507d47346da8ceb0e0d9e655d7206
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-05-26 22:37:16 +0200

    Test 2 layer model.

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -522,8 +522,8 @@ class GroundwaterModflow(object):
         # - pass the values to the RIV package 
         self.pcr_modflow.setRiver(self.surface_water_elevation, \
                                   self.surface_water_bed_elevation, self.bed_conductance, 2)
-        self.pcr_modflow.setRiver(self.surface_water_elevation, \
-                                  self.surface_water_bed_elevation, self.bed_conductance, 1)
+        #~ self.pcr_modflow.setRiver(self.surface_water_elevation, \
+                                  #~ self.surface_water_bed_elevation, self.bed_conductance, 1)
 
         # TODO: Improve this concept, particularly while calculating surface water elevation in lakes and reservoirs
         
