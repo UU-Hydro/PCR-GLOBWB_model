d95d890ee41b85288d3e9b1ee23f7ae0e5bbefc1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-05-28 01:46:18 +0200

    Finalizing before re-run.

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -342,7 +342,7 @@ class GroundwaterModflow(object):
                            currTimeStep = None,\
                            PERLEN = 1.0, 
                            NSTP   = 1, \
-                           HCLOSE = 0.1,\
+                           HCLOSE = 0.25,\
                            RCLOSE = 10.* 400.*400.,\
                            MXITER = 300,\
                            ITERI = 100,\
