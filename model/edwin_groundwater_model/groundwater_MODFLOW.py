30c59f5b2f56371f536146989ab1c3c4beec58cb
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-05-28 19:35:50 +0200

    Runs for cartesius.

--- model/edwin_groundwater_model/groundwater_MODFLOW.py
+++ model/edwin_groundwater_model/groundwater_MODFLOW.py
@@ -368,8 +368,8 @@ class GroundwaterModflow(object):
                            NSTP   = 1, \
                            HCLOSE = 1.0,\
                            RCLOSE = 10.* 400.*400.,\
-                           MXITER = 150,\
-                           ITERI = 50,\
+                           MXITER = 50,\
+                           ITERI = 30,\
                            NPCOND = 1,\
                            RELAX = 1.00,\
                            NBPOL = 2,\
