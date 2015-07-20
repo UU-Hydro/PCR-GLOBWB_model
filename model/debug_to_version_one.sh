430b9848dbc82d2c311f11ce33d9b0588c62b4d3
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-20 20:29:27 +0200

    Test the oldcalc script.

--- model/debug_to_version_one.sh
+++ model/debug_to_version_one.sh
@@ -1,2 +1,13 @@
 # copying the script and tbl
-cp ../pcrglobwb_v1/pcrglobwb_v1.2_fullforcing_edwinVersion.txt /scratch/edwin/test_RhineMeuse30min_EFAS_test
+cp ../pcrglobwb_v1/pcrglobwb_v1.2_fullforcing_edwinVersion.txt /scratch/edwin/test_RhineMeuse30min_EFAS_test/maps
+cp ../pcrglobwb_v1/param_permafrost_edwinVersion.tbl /scratch/edwin/test_RhineMeuse30min_EFAS_test/maps
+
+# go to the maps directory and make the results directory
+cd /scratch/edwin/test_RhineMeuse30min_EFAS_test/maps
+mkdir results
+
+# execute the script
+oldcalc -f pcrglobwb_v1.2_fullforcing_edwinVersion.txt 366
+
+
+
