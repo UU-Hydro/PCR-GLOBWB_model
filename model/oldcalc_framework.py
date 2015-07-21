948716af6f83933e83f83d7e18e1d276bc199c4d
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-07-21 15:45:47 +0200

    Test with debugging.

---
+++ model/oldcalc_framework.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import shutil
+import datetime
+
+import pcraster as pcr
+from pcraster.framework import DynamicModel
+
+from ncConverter import *
+import virtualOS as vos
+
+import variable_list as varDicts
+
+import logging
+logger = logging.getLogger(__name__)
+
+class PCRGlobWBVersionOne(DynamicModel):
+
+    def __init__(self, configuration, \
+                       modelTime):
+        DynamicModel.__init__(self)
+        
+        # configuration (based on the ini file)
+        self.configuration = configuration
+        
+        # time variable/object
+        self.modelTime = modelTime
+        
+        # cloneMapFileName
+        self.cloneMapFileName = self.configuration.globalOptions['cloneMap']
+        pcr.setclone(self.cloneMapFileName)
+        
+        # output variables that will be compared (at daily resolution)
+        self.debug_variables = ['runoff',
+                                'totalEvaporation',
+                                'storUppTotal',
+                                'storLowTotal',
+                                'actBareSoilEvap',
+                                'directRunoff',
+                                'gwRecharge',
+                                'baseflow',
+                                'precipitation',
+                                'temperature',
+                                'referencePotETP']
+        
+        # folder/location of oldcalc input maps
+        self.maps_folder = self.configuration.mapsDir
+        
+        # folder/location of oldcalc results maps
+        self.results_folder = self.configuration.globalOptions['outputDir']+"/oldcalc_results/"
+        # - preparing the directory
+        if os.path.exists(self.results_folder): shutil.rmtree(self.results_folder)
+        os.makedirs(self.results_folder)
+        # - preparing the folder to store netcdf files
+        self.netcdf_folder  = self.configuration.globalOptions['outputDir']+"/oldcalc_results/netcdf/"
+        os.makedirs(self.netcdf_folder)
+        
+        # go to the starting directory and copy/backup the oldcalc_script and parameter_table files 
+        os.chdir(self.configuration.starting_directory)
+        
+        # the oldscript scripts used:
+        self.oldcalc_script_file  = vos.getFullPath(self.configuration.globalOptions['oldcalc_script_file'],  self.configuration.starting_directory)
+        self.parameter_tabel_file = vos.getFullPath(self.configuration.globalOptions['parameter_tabel_file'], self.configuration.starting_directory)
+        
+        # make the backup of oldscript scripts used
+        shutil.copy(self.oldcalc_script_file , self.configuration.scriptDir)
+        shutil.copy(self.parameter_tabel_file, self.configuration.scriptDir)
+
+        # attribute information used in netcdf files:
+        netcdfAttributeDictionary = {}
+        netcdfAttributeDictionary['institution'] = self.configuration.globalOptions['institution'] 
+        netcdfAttributeDictionary['title'      ] = "PCR-GLOBWB 1 output"
+        netcdfAttributeDictionary['description'] = self.configuration.globalOptions['description']+" (this is the output from the oldcalc PCR-GLOBWB version 1)"
+        
+        # netcdf object for reporting
+        self.netcdf_report = PCR2netCDF(configuration, netcdfAttributeDictionary)       
+
+        # make/prepare netcdf files
+        for var in self.debug_variables:
+
+            short_name = varDicts.netcdf_short_name[var]
+            unit       = varDicts.netcdf_unit[var]      
+            long_name  = varDicts.netcdf_long_name[var]
+            if long_name == None: long_name = short_name  
+
+            netcdf_file_name = self.netcdf_folder+"/"+short_name+"_dailyTot_output_version_one.nc"
+
+            logger.info("Creating the netcdf file for daily reporting for the variable %s to the file %s (output from PCR-GLOBWB version 1).", str(var), str(netcdf_file_name))
+
+            self.netcdfObj.createNetCDF(netcdf_file_name,short_name,unit,long_name)
+        
+    def initial(self): 
+        
+        logger.info("Execute the oldcalc script.")
+        
+        # choosing the steps for monthly reporting
+        if self.modelTime.nrOfTimeSteps == 365: monthly_end_times = "31 59 90 120 151 181 212 243 273 304 334 365"
+        if self.modelTime.nrOfTimeSteps == 366: monthly_end_times = "31 60 91 121 152 182 213 244 274 305 335 366"
+        
+        # execute oldcalc script
+        # - copy the parameter table file to mapsDir
+        shutil.copy(self.parameter_tabel_file, self.configuration.mapsDir)
+        # - copy the script directory to outputDir and execute it from there
+        shutil.copy(self.oldcalc_script_file , self.configuration.globalOptions['outputDir'])
+        os.chdir(self.configuration.globalOptions['outputDir'])
+        # - execute the script
+        cmd = 'oldcalc -f '+str(os.path.basename(self.oldcalc_script_file))+" "+monthly_end_times
+        vos.cmd_line(cmd)
+
+    def dynamic(self):
+        
+        # make netcdf files, particularly for daily resolution values 
+        # logging average values to the log file
+        
+        # re-calculate current model time using current pcraster timestep value
+        self.modelTime.update(self.currentTimeStep())
+
+        # timeStamp for reporting
+        timeStamp = datetime.datetime(self.modelTime.year,\
+                                      self.modelTime.month,\
+                                      self.modelTime.day,0)
+
+        # reading variables from pcraster files and report them as netcdf files
+        for var in self.debug_variables:
+
+            pcraster_map_file_name = self.results_folder + "/" +\
+                                     pcr.framework.frameworkBase.generateNameT(varDicts.pcr_short_name[var],\
+                                                                               self.modelTime.timeStepPCR)
+            logger.dbg("Reading the variable %s from the file %s ", var, pcraster_map_file_name)
+            pcr_map_values = pcr.readmap(str(pcraster_map_file_name))
+            
+            netcdf_file_name = self.netcdf_folder+"/"+short_name+"_dailyTot_output_version_one.nc"
+
+            logger.dbg("Saving to the file %s ", netcdf_file_name)
+            self.netcdf_report.data2NetCDF(self.netcdf_folder,\
+                                           varDicts.netcdf_short_name[var],\
+                                           pcr.pcr2numpy(pcr_map_values,vos.MV),\
+                                           timeStamp)
