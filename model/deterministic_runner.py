f56228a128403e9dc62e6ffd07e724689ff94c1c
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-07-21 15:54:48 +0200

    Test with debugging.

--- model/deterministic_runner.py
+++ model/deterministic_runner.py
@@ -62,45 +62,45 @@ def main():
     # timeStep info: year, month, day, doy, hour, etc
     currTimeStep = ModelTime() 
     
-    # object for spin_up
-    spin_up = SpinUp(configuration)            
+    #~ # object for spin_up
+    #~ spin_up = SpinUp(configuration)            
 
-    # spinningUp
-    noSpinUps = int(configuration.globalOptions['maxSpinUpsInYears'])
-    initial_state = None
-    if noSpinUps > 0:
-        
-        logger.info('Spin-Up #Total Years: '+str(noSpinUps))
-
-        spinUpRun = 0 ; has_converged = False
-        while spinUpRun < noSpinUps and has_converged == False:
-            spinUpRun += 1
-            currTimeStep.getStartEndTimeStepsForSpinUp(
-                    configuration.globalOptions['startTime'],
-                    spinUpRun, noSpinUps)
-            logger.info('Spin-Up Run No. '+str(spinUpRun))
-            deterministic_runner = DeterministicRunner(configuration, currTimeStep, initial_state)
-            
-            all_state_begin = deterministic_runner.model.getAllState() 
-            
-            dynamic_framework = DynamicFramework(deterministic_runner,currTimeStep.nrOfTimeSteps)
-            dynamic_framework.setQuiet(True)
-            dynamic_framework.run()
-            
-            all_state_end = deterministic_runner.model.getAllState() 
-            
-            has_converged = spin_up.checkConvergence(all_state_begin, all_state_end, spinUpRun, deterministic_runner.model.routing.cellArea)
-            
-            initial_state = deterministic_runner.model.getState()
-    
-    # Running the deterministic_runner (excluding DA scheme)
-    currTimeStep.getStartEndTimeSteps(configuration.globalOptions['startTime'],
-                                      configuration.globalOptions['endTime'])
-    logger.info('Transient simulation run started.')
-    deterministic_runner = DeterministicRunner(configuration, currTimeStep, initial_state)
-    dynamic_framework = DynamicFramework(deterministic_runner,currTimeStep.nrOfTimeSteps)
-    dynamic_framework.setQuiet(True)
-    dynamic_framework.run()
+    #~ # spinningUp
+    #~ noSpinUps = int(configuration.globalOptions['maxSpinUpsInYears'])
+    #~ initial_state = None
+    #~ if noSpinUps > 0:
+        #~ 
+        #~ logger.info('Spin-Up #Total Years: '+str(noSpinUps))
+#~ 
+        #~ spinUpRun = 0 ; has_converged = False
+        #~ while spinUpRun < noSpinUps and has_converged == False:
+            #~ spinUpRun += 1
+            #~ currTimeStep.getStartEndTimeStepsForSpinUp(
+                    #~ configuration.globalOptions['startTime'],
+                    #~ spinUpRun, noSpinUps)
+            #~ logger.info('Spin-Up Run No. '+str(spinUpRun))
+            #~ deterministic_runner = DeterministicRunner(configuration, currTimeStep, initial_state)
+            #~ 
+            #~ all_state_begin = deterministic_runner.model.getAllState() 
+            #~ 
+            #~ dynamic_framework = DynamicFramework(deterministic_runner,currTimeStep.nrOfTimeSteps)
+            #~ dynamic_framework.setQuiet(True)
+            #~ dynamic_framework.run()
+            #~ 
+            #~ all_state_end = deterministic_runner.model.getAllState() 
+            #~ 
+            #~ has_converged = spin_up.checkConvergence(all_state_begin, all_state_end, spinUpRun, deterministic_runner.model.routing.cellArea)
+            #~ 
+            #~ initial_state = deterministic_runner.model.getState()
+    #~ 
+    #~ # Running the deterministic_runner (excluding DA scheme)
+    #~ currTimeStep.getStartEndTimeSteps(configuration.globalOptions['startTime'],
+                                      #~ configuration.globalOptions['endTime'])
+    #~ logger.info('Transient simulation run started.')
+    #~ deterministic_runner = DeterministicRunner(configuration, currTimeStep, initial_state)
+    #~ dynamic_framework = DynamicFramework(deterministic_runner,currTimeStep.nrOfTimeSteps)
+    #~ dynamic_framework.setQuiet(True)
+    #~ dynamic_framework.run()
 
 
     # for debugging to PCR-GLOBWB version one
