e174b944f8f9a3b8a0fbb74c523ff8b8df60ea4a
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-23 22:34:09 +0200

    Ignore snow.

--- model/landCover.py
+++ model/landCover.py
@@ -880,7 +880,7 @@ class LandCover(object):
                                     self.throughfall)                    # original Rens line: INTS_L[TYPE] = max(0,INTS_L[TYPE]+PRPTOT-PRP)
          
         # partitioning throughfall into snowfall and liquid Precipitation:
-        estimSnowfall = pcr.ifthenelse(meteo.temperature <= self.freezingT, \
+        estimSnowfall = pcr.ifthenelse(meteo.temperature < self.freezingT, \
                                        meteo.precipitation, 0.0)         # original Rens line: SNOW = if(TA<TT,PRPTOT,0)
                                                                          # But Rens put it in his "meteo" module in order to allow snowfallCorrectionFactor (SFCF).
         #
@@ -962,7 +962,8 @@ class LandCover(object):
                     pcr.max(meteo.temperature - self.freezingT, 0.0) * \
                     self.degreeDayFactor)*1.0*1.0)                      # DSC[TYPE] = if(TA<=TT,CFR*SCF_L[TYPE],
                                                                         #                      -min(SC_L[TYPE],max(TA-TT,0)*CFMAX*Duration*timeslice()))
-
+        deltaSnowCover = pcr.roundoff(deltaSnowCover * 1000.)/1000.
+        
         # for reporting snow melt in m/day
         self.snowMelt = pcr.ifthenelse(deltaSnowCover < 0.0, deltaSnowCover * pcr.scalar(-1.0), pcr.scalar(0.0))
         
