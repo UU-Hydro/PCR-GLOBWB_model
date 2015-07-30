9b09b0df074c66c38bd3fc2e6c9ff76793db700f
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-07-30 18:45:53 +0200

    First test.

--- model/landCover.py
+++ model/landCover.py
@@ -267,7 +267,8 @@ class LandCover(object):
                     vars()[var] = vos.netcdf2PCRobjCloneWithoutTime(\
                                         landCoverPropertiesNC, var, \
                                         cloneMapFileName = self.cloneMap)
-                    vars()[var] = pcr.cover(vars()[str(var)], 0.0)
+                    pietje
+                    vars()[var] = pcr.cover(vars()[var], 0.0)
 
             # The parameter arnoBeta for the Improved Arno's scheme:
             # - There are three ways in defining arnoBeta. The ranks below indicate their priority:
