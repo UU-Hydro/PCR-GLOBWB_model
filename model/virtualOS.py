831df034cac146d55a8cf9aa328000f048b71320
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-08-04 16:46:19 +0200

    Continue working for 6 land cover classes.

--- model/virtualOS.py
+++ model/virtualOS.py
@@ -519,7 +519,7 @@ def netcdf2PCRobjClone(ncFile,varName,dateInput,\
     # flip cropData if f.variables['lat'][0] < f.variables['lat'][1] 
     if we_have_to_flip: 
         cropData = cropData[:]
-        cropData = np.flipud(cropData[:]) 
+        cropData = np.flipud(cropData) 
         #~ test = cropData
         #~ cropData = test[::-1,...]  
 
