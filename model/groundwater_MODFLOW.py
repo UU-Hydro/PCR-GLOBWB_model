d05a86205cd1e8356a773dedf67d9bfbcd3e102b
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-11-03 22:40:25 +0100

    Checking for steady state modflow.

--- model/groundwater_MODFLOW.py
+++ model/groundwater_MODFLOW.py
@@ -1060,10 +1060,10 @@ class GroundwaterModflow(object):
             surface_water_bed_elevation = pcr.ifthen(pcr.scalar(self.WaterBodies.waterBodyIds) > 0.0, \
                                                      self.dem_riverbed - additional_depth)
             #
-            # - for lakes and resevoirs, estimate bed elevation from dem and bankfull depth
-            surface_water_bed_elevation  = pcr.ifthen(pcr.scalar(self.WaterBodies.waterBodyIds) > 0.0, self.dem_average)
-            surface_water_bed_elevation  = pcr.areaaverage(surface_water_bed_elevation, self.WaterBodies.waterBodyIds)
-            surface_water_bed_elevation -= pcr.areamaximum(self.bankfull_depth, self.WaterBodies.waterBodyIds) 
+            #~ # - for lakes and resevoirs, estimate bed elevation from dem and bankfull depth
+            #~ surface_water_bed_elevation  = pcr.ifthen(pcr.scalar(self.WaterBodies.waterBodyIds) > 0.0, self.dem_average)
+            #~ surface_water_bed_elevation  = pcr.areaaverage(surface_water_bed_elevation, self.WaterBodies.waterBodyIds)
+            #~ surface_water_bed_elevation -= pcr.areamaximum(self.bankfull_depth, self.WaterBodies.waterBodyIds) 
             #
             surface_water_bed_elevation  = pcr.cover(surface_water_bed_elevation, self.dem_riverbed)
             #
