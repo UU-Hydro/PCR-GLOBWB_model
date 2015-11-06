ca502b751ff6f412cd6e3f98bebf446264a32b30
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-11-06 22:25:16 +0100

    Another steady state model.

--- model/groundwater_MODFLOW.py
+++ model/groundwater_MODFLOW.py
@@ -1053,6 +1053,7 @@ class GroundwaterModflow(object):
             # - rounding values for surface_water_bed_elevation
             self.surface_water_bed_elevation = pcr.roundup(surface_water_bed_elevation * 1000.)/1000.
 
+            
             #~ ############################################################################################################################################
             #~ # - bed surface area (unit: m2)
             #~ bed_surface_area = pcr.ifthen(pcr.scalar(self.WaterBodies.waterBodyIds) > 0.0, \
