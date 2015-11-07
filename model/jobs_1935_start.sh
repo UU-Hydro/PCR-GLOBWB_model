1e87ef68f379d9edbac7e8f29e7e9435a63a3789
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-11-08 00:17:08 +0100

    Make a run starting from 1935.

--- model/jobs_1935_start.sh
+++ model/jobs_1935_start.sh
@@ -3,4 +3,4 @@
 #SBATCH -t 119:00:00 
 #SBATCH -p fat                                                                                                                                                                              
 
-python ../model/couplingPCR-GLOBWB-MODFLOW.py ../config/prepare_5min_run/setup_05min_pcrglobwb-modflow_cartesius_from_1935.ini no_debug
+python couplingPCR-GLOBWB-MODFLOW.py ../config/prepare_5min_run/setup_05min_pcrglobwb-modflow_cartesius_from_1935.ini no_debug
