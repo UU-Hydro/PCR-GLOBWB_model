46c922b90378cb97e32f052209c40a4f8b7c4d4b
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-11-08 00:10:42 +0100

    Make a run starting from 1935.

--- jobs_long_run/1935_start.sh
+++ jobs_long_run/1935_start.sh
@@ -1,6 +1,6 @@
-#!/bin/bash                                                                                                                                                                                 
-#SBATCH -N 1                                                                                                                                                                                
-#SBATCH -t 119:00:00                                                                                                                                                                         
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:00:00 
 #SBATCH -p fat                                                                                                                                                                              
 
 python ../model/couplingPCR-GLOBWB-MODFLOW.py ../config/prepare_5min_run/setup_05min_pcrglobwb-modflow_cartesius_from_1935.ini no_debug
