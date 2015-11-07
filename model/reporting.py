40af1b846848d233e97329743bb0198261e9ad30
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-11-07 21:43:24 +0100

    First test to cartesius.

--- model/reporting.py
+++ model/reporting.py
@@ -464,9 +464,9 @@ class Reporting(object):
     def basic_post_processing(self):
 
         # forcing 
-        self.precipitation  = pcr.ifthen(self.landmask, self._model.meteo.precipitation) 
-        self.temperature    = pcr.ifthen(self.landmask, self._model.meteo.temperature)
-        self.referencePotET = pcr.ifthen(self.landmask, self._model.meteo.referencePotET) 
+        self.precipitation  = pcr.ifthen(self._model.routing.landmask, self._model.meteo.precipitation) 
+        self.temperature    = pcr.ifthen(self._model.routing.landmask, self._model.meteo.temperature)
+        self.referencePotET = pcr.ifthen(self._model.routing.landmask, self._model.meteo.referencePotET) 
 
         # potential and actual evaporation from land surface part (m)
         self.totalLandSurfacePotET = self._model.landSurface.totalPotET 
