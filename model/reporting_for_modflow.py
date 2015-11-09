c6b27271a45ea9dc55c4a2a4dde850ae38572aa4
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-11-09 21:59:43 +0100

    test Java.

--- model/reporting_for_modflow.py
+++ model/reporting_for_modflow.py
@@ -275,7 +275,7 @@ class Reporting(object):
         # - initiate the (accumulated) volume rate (m3/day) (for accumulating the fluxes from all layers)
         totalBaseflowVolumeRate = pcr.scalar(0.0) 
         # - accumulating fluxes from all layers
-        for i in range(1, self.number_of_layers+1):
+        for i in range(1, self._model.modflow.number_of_layers+1):
             # from the river leakage
             var_name = 'riverLeakageLayer'+str(i)
             totalBaseflowVolumeRate += pcr.cover(vars(self._model.modflow)[var_name], 0.0)
