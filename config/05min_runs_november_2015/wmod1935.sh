9d26d589b1ad9eff290ead89e23cc64b38b49a9e
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-11-11 01:30:30 +0100

    a coupled run from 1935, 1950, 1975, 1990

---
+++ config/05min_runs_november_2015/wmod1935.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:00:00 
+#SBATCH -p fat                                                                                                                                                                              
+
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python couplingPCR-GLOBWB-MODFLOW.py ../config/05min_runs_november_2015/setup_05min_pcrglobwb-modflow_cartesius_from_1935.ini no_debug
+
+# pcrglobwb modflow - start from 1935
