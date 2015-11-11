a98dec5a550e7065b01acf6775db19b898d3a925
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-11-11 19:18:47 +0100

    Preparing the 5min runs with 4 land covers.

--- config/05min_runs_november_2015/nmod1901.sh
+++ config/05min_runs_november_2015/nmod1901.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -t 119:00:00 
-#SBATCH -p normal                                                                                                                                                                              
+#SBATCH -p fat                                                                                                                                                                              
 
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
 python parallelPCR-GLOBWB.py ../config/05min_runs_november_2015/setup_05min_pcrglobwb_only_cartesius_parallel.ini no_debug
--- config/05min_runs_november_2015/wmod1901.sh
+++ config/05min_runs_november_2015/wmod1901.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -t 119:00:00 
-#SBATCH -p normal                                                                                                                                                                              
+#SBATCH -p fat                                                                                                                                                                              
 
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
 python couplingPCR-GLOBWB-MODFLOW.py ../config/05min_runs_november_2015/setup_05min_pcrglobwb-modflow_cartesius.ini no_debug
