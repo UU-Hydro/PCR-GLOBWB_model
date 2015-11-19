c8e891a0d52779855030f61e924f7ec467d24b85
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-11-19 15:20:14 +0100

    Continue long runs.

---
+++ config/05min_runs_november_2015/continue_on_19_november/nmkw1958_global_one.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:00:00 
+#SBATCH -p fat                                                                                                                                                                              
+
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallelPCR-GLOBWB.py ../config/05min_runs_november_2015/continue_on_19_november/setup_05min_pcrglobwb_only_cartesius_parallel_4LCs_KW_continue_global_one.ini no_debug
+
+# pcrglobwb only (without modflow) - with kinematic wave - start/continue from 1958 - but only for the clones: M17,M19,M26,M13,M18,M20,M05,M03,M46,M21,M27,M49,M16,M44,M52,M25,M09,M08,M11,M42,M12,M39,M07,M15,M38,M48,M40,M41,M22,M14
+
