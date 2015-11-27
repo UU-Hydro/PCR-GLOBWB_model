5c46143df655c9ca8e3447c89ea64a3d95480f45
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-11-27 19:13:03 +0100

    Continue long runs.

--- config/05min_runs_november_2015/continue_on_27_november/with_modflow/wmod1901_cont1969.sh
+++ config/05min_runs_november_2015/continue_on_27_november/with_modflow/wmod1901_cont1969.sh
@@ -4,6 +4,6 @@
 #SBATCH -p fat                                                                                                                                                                              
 
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
-python couplingPCR-GLOBWB-MODFLOW.py ../config/05min_runs_november_2015/continue_on_27_november/setup_05min_pcrglobwb-modflow_cartesius_from_1969_continue.ini no_debug
+python couplingPCR-GLOBWB-MODFLOW.py ../config/05min_runs_november_2015/continue_on_27_november/with_modflow/setup_05min_pcrglobwb-modflow_cartesius_from_1969_continue.ini no_debug
 
 # pcrglobwb modflow - start from 1901 - continue from 1920 - continue from 1933 - continue from 1955 - continue from 1969
