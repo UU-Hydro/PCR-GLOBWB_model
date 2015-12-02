6a2cd89643430799893217f291fd252a6682a1a9
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-12-03 00:09:27 +0100

    Continue long runs.

---
+++ config/05min_runs_november_2015/continue_on_02_december/nmkw1958_cont1991_global_two.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:00:00 
+#SBATCH -p normal                                                                                                                                                                              
+#SBATCH --constraint=haswell
+
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallelPCR-GLOBWB.py ../config/05min_runs_november_2015/continue_on_02_december/setup_05min_pcrglobwb_only_cartesius_parallel_4LCs_KW_continue_from_1991_global_two.ini no_debug
+
+# pcrglobwb only (without modflow) - with kinematic wave - start/continue from 1958 - but only for the clones: M23,M51,M04,M06,M10,M02,M45,M50,M47,M35,M24,M01,M36,M53,M33,M43,M34,M37,M31,M32,M28,M30,M29
+#                                                                continue from 1973 - but only for the clones: M23,M51,M04,M06,M10,M02,M45,M50,M47,M35,M24,M01,M36,M53,M33,M43,M34,M37,M31,M32,M28,M30,M29
+#                                                                continue from 1991 - but only for the clones: M23,M51,M04,M06,M10,M02,M45,M50,M47,M35,M24,M01,M36,M53,M33,M43,M34,M37,M31,M32,M28,M30,M29
