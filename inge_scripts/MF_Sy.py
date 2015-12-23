63d8ebd6dfabc37848f7d37d1ce1396d95b6ce61
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-12-23 19:24:38 +0100

    Add necessary files for Inge.

--- inge_scripts/MF_Sy.py
+++ inge_scripts/MF_Sy.py
@@ -410,10 +410,11 @@ class mymodflow(DynamicModel):
 			# reporting to netcdf files
 			for variable in self.variable_output:
 				chosenVarField = pcr2numpy(self.__getattribute__(variable), vos.MV)
-				self.netcdfReport.data2NetCDF(ncFile = str(self.outDir) + self.netcdf_output["file_name"][variable],\
-										varName = variable,\
-										varField = chosenVarField,
-										timeStamp = timeStamp)
+				self.netcdfReport.data2NetCDF(
+				                        str(self.outDir) + self.netcdf_output["file_name"][variable],\
+										variable, \
+										chosenVarField, \
+										timeStamp)
 
 cloneMap 	 = "/projects/0/dfguu/users/inge/inputMAPS/Clone_05min.map" # "../MFinp/australia/australia_clone.map" "../../PCR-GLOBWB/MFinp/australia/australia_clone.map" #
 strStartTime = sys.argv[1]
