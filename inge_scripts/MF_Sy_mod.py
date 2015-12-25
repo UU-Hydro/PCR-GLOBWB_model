af86881df4ee830c1f3c2afb2b62102fb585655e
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-12-25 17:15:20 +0100

    Try again for Inge.

--- inge_scripts/MF_Sy_mod.py
+++ inge_scripts/MF_Sy_mod.py
@@ -270,7 +270,8 @@ class mymodflow(DynamicModel):
 		
 			pcr_modflow = None
 			pcr_modflow = pcraster_modflow.PCRasterModflow(self.cloneMap)
-			pcr_modflow.initialize(self, self.modelTime, \
+			pcr_modflow.initialize(
+			             self.modelTime, \
 	                     self.input_bottom_l1, self.self.input_top_l1, self.input_top_l2, \
 	                     self.input_ibound, \
 	                     self.input_khoriz_l1, self.input_kvert_l1, \
