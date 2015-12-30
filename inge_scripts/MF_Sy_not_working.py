f851cf803d8869b93537a329ceff5612452cd175
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-12-30 14:59:52 +0100

    Test again.

--- inge_scripts/MF_Sy_not_working.py
+++ inge_scripts/MF_Sy_not_working.py
@@ -137,7 +137,7 @@ class mymodflow(DynamicModel):
 													self.cloneMap, self.tmpDir)
 															
 		conflayers			=	pcr.boolean(\
-								vos.readPCRmapClone("/projects/0/dfguu/users/inge/inputMAPS/aquifer_parameters_MF/conflayers4.map",\				
+								vos.readPCRmapClone("/projects/0/dfguu/users/inge/inputMAPS/aquifer_parameters_MF/conflayers4.map",\
 													self.cloneMap, self.tmpDir), 0.0)	
 		ksat_log			=	vos.readPCRmapClone("/projects/0/dfguu/users/inge/inputMAPS/aquifer_parameters_MF/lkmc_ave.map",\
 													self.cloneMap, self.tmpDir)		
