6d1c2fa60647c70c0c348c7dc702e1595f2d3259
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-12-30 15:32:48 +0100

    Test again.

--- inge_scripts/virtualOS.py
+++ inge_scripts/virtualOS.py
@@ -895,7 +895,7 @@ def gdalwarpPCR(input,output,cloneOut,tmpDir,isLddMap=False,isNominalMap=False,i
     co = 'gdal_translate -of PCRaster '+ \
               str(tmpDir)+'tmp_out.tif '+str(output)
     if isBooleanMap:
-        co = 'pcrcalc '+str(output)+' = "scalar('+str(tmpDir)+'tmp_out.tif ' +')"'
+        co = 'pcrcalc "'+str(output)+'" = "scalar('+str(tmpDir)+'tmp_out.tif ' +')"'
         print(co)
     cOut,err = subprocess.Popen(co, stdout=subprocess.PIPE,stderr=open(os.devnull),shell=True).communicate()
     # 
