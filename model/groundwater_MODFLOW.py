42def580d1bccd0095d87c309a70df9f4ac699ba
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-01-04 20:37:45 +0100

    Try to improve MODFLOW.

--- model/groundwater_MODFLOW.py
+++ model/groundwater_MODFLOW.py
@@ -459,8 +459,8 @@ class GroundwaterModflow(object):
         primary = pcr.cover(self.specificYield * self.cellAreaMap/(pcr.clone().cellSize()*pcr.clone().cellSize()), 0.0)
         primary = pcr.max(1e-20, primary)
         secondary = pcr.max(1e-5, primary * 0.001)         # dummy values as we used layer type 00
-        self.pcr_modflow.setStorage(primary, secondary, 1)
-        self.pcr_modflow.setStorage(primary, secondary, 2)
+        self.pcr_modflow.setStorage(primary * 0.1, secondary, 1)
+        self.pcr_modflow.setStorage(primary      , secondary, 2)
 
         # specification for conductivities (BCF package)
         horizontal_conductivity = self.kSatAquifer # unit: m/day
