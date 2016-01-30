408e6c86c3f396e1e0785a5e69bbf10ff116f338
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-01-30 13:07:35 +0100

    Continue natural run with kinematic wave from 1966.

---
+++ config/05min_runs_january_2016/nmod1958_natural_kinematic_wave_edwin_continue_from_1966.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00
+#SBATCH -p fat
+
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallelPCR-GLOBWB_without_prefactors.py ../config/05min_runs_january_2016/setup_05min_pcrglobwb_only_cartesius_parallel_natural_kinematic_wave_continue_from_1966.ini no_debug
+
+# pcrglobwb only (natural, kinematic wave, without modflow) - start
+# pcrglobwb only (natural, kinematic wave, without modflow) - continue from 1958
+# pcrglobwb only (natural, kinematic wave, without modflow) - continue from 1966
+
