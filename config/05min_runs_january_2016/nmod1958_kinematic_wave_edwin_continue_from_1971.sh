e873d99e15ba7b2ad010da263e350eada8b780c3
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-02-10 00:10:37 +0100

    I made a stupid mistake in storGroundwaterFossil for 4LC run with kinematic wave.

--- config/05min_runs_january_2016/nmod1958_kinematic_wave_edwin_continue_from_1971.sh
+++ config/05min_runs_january_2016/nmod1958_kinematic_wave_edwin_continue_from_1971.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -t 119:59:00 
-#SBATCH -p normal                                                                                                                                                                              
+#SBATCH -p fat                                                                                                                                                                              
 
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
 python parallelPCR-GLOBWB_without_prefactors.py ../config/05min_runs_january_2016/setup_05min_pcrglobwb_only_cartesius_parallel_4LCs_edwin_parameter_set_kinematic_wave_continue_from_1971.ini no_debug
