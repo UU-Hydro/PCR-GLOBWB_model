daf89f4150bf2be1a50ef028b9ff6ac5a41ab334
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-02-26 12:45:48 +0100

    Test run for nmod1901_4LCs_edwin_parameter_set_no_correction.sh.

--- model/parallel_pcrglobwb_with_prefactors.py
+++ model/parallel_pcrglobwb_with_prefactors.py
@@ -18,9 +18,8 @@ logger = logging.getLogger(__name__)
 # get the configuration/ini file given in the system argument
 iniFileName = os.path.abspath(sys.argv[1])
 
-# option for debugging
-debug_option = "False"
-if len(sys.argv) > 2: debug_option = str(sys.argv[2])
+# option for debuging and paralelization 
+debug_option = str(sys.argv[2])
 
 # object to handle configuration/ini file
 generalConfiguration = configuration.Configuration(iniFileName = iniFileName, debug_mode = False, no_modification = False)
@@ -54,7 +53,7 @@ path_of_this_module = os.path.abspath(os.path.dirname(__file__))
 # working/starting directory where all script files are used
 for filename in glob.glob(os.path.join(path_of_this_module, '*.py')):
     shutil.copy(filename, scriptDir)
-os.chdir(scriptDir)
+os.chdir(scriptDir)    
 
 # pcr-globwb clone areas (for pcr-globwb multiple runs)
 clone_codes = list(set(generalConfiguration.globalOptions['cloneAreas'].split(",")))
@@ -62,21 +61,21 @@ if clone_codes[0] == "Global": clone_codes = ['M%02d'%i for i in range(1,54,1)]
 
 # TODO: run a steady state modflow to estimate the initial values for relativeGroundwaterHead, storGroundwater and baseflow
 
-# command lines for PCR-GLOBWB
+# command lines for PCR-GLOBWB 
 i_clone = 0
 cmd = ''
 for clone_code in clone_codes:
 
-   cmd += "python deterministic_runner_parallel.py " + iniFileName +" "+\
-                                                      debug_option +" "+\
-                                                        clone_code +" "
+   cmd += "deterministic_runner_glue_with_parallel_option_february_2016.py " + iniFileName +" "+\
+                                                                               debug_option +" "+\
+                                                                               clone_code +" "
    cmd = cmd+" & "
    i_clone += 1
 
 # don't foget to add the following line
-cmd = cmd + "wait"
+cmd = cmd + "wait"       
 
 print cmd
 
-# execute PCR-GLOBWB and MODFLOW
-vos.cmd_line(cmd, using_subprocess = False)
+#~ # execute PCR-GLOBWB and MODFLOW
+#~ vos.cmd_line(cmd, using_subprocess = False)      
