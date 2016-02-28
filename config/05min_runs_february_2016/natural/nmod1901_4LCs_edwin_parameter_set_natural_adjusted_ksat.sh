20b29981ad8e6459f644377706368598de6cf130
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-02-28 16:43:51 +0100

    Re-run all natural runs.

--- config/05min_runs_february_2016/natural/nmod1901_4LCs_edwin_parameter_set_natural_adjusted_ksat.sh
+++ config/05min_runs_february_2016/natural/nmod1901_4LCs_edwin_parameter_set_natural_adjusted_ksat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 119:59:00 
+#SBATCH -t 102:00:00 
 #SBATCH -p fat                                                                                                                                                                              
 
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
