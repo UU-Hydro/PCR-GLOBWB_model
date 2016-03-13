47552a2355f3e87531bebcc3cbc908cb4a7388fc
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-03-13 08:56:45 +0100

    To run nmod1901_6LCs_original_parameter_set_natural_adjuested_ksat_continue_from_1923_edwinhs.sh and nmod1901_6LCs_original_parameter_set_natural_continue_from_1922_edwinhs.sh. - using normal nodes

---
+++ config/05min_runs_february_2016/continue_11_march_2016/non-natural/nmod1901_6LCs_original_parameter_set_adjuested_ksat_continue_from_1923_edwinhs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal                                                                                                                                                                              
+
+cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_with_prefactors.py ../config/05min_runs_february_2016/continue_11_march_2016/non-natural/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_only_cartesius_parallel_6LCs_original_parameter_set_adjusted_ksat_continue_from_1923.ini parallel 
+
+# pcrglobwb only (with accutraveltime, without modflow) - start
+#                                                         continue from 1923 (using normal node)
+
