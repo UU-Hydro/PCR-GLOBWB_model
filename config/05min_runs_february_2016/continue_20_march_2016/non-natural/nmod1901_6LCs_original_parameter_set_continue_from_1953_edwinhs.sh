b9e46a130649c6653bb4ec350a4593e6d823e7cb
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-03-20 20:18:02 +0100

    Continue nmod1901_6LCs_original_parameter_set_continue_from_1953_edwinhs.sh.

--- config/05min_runs_february_2016/continue_20_march_2016/non-natural/nmod1901_6LCs_original_parameter_set_continue_from_1953_edwinhs.sh
+++ config/05min_runs_february_2016/continue_20_march_2016/non-natural/nmod1901_6LCs_original_parameter_set_continue_from_1953_edwinhs.sh
@@ -4,7 +4,7 @@
 #SBATCH -p fat                                                                                                                                                                              
 
 cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
-python parallel_pcrglobwb_with_prefactors.py ../config/05min_runs_february_2016/continue_11_march_2016/non-natural/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_only_cartesius_parallel_6LCs_original_parameter_set_continue_from_1953.ini parallel 
+python parallel_pcrglobwb_with_prefactors.py ../config/05min_runs_february_2016/continue_17_march_2016/non-natural/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_only_cartesius_parallel_6LCs_original_parameter_set_continue_from_1953.ini parallel 
 
 # pcrglobwb only (with accutraveltime, without modflow) - start
 #                                                         continue from 1922
