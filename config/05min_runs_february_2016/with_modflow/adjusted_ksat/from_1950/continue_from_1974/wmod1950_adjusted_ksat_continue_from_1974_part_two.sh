b01e057c438a27ec29e19bcb8f43481c22a0c0e3
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-31 15:12:45 +0200

    Try with two nodes.

--- config/05min_runs_february_2016/with_modflow/adjusted_ksat/from_1950/continue_from_1974/wmod1950_adjusted_ksat_continue_from_1974_part_two.sh
+++ config/05min_runs_february_2016/with_modflow/adjusted_ksat/from_1950/continue_from_1974/wmod1950_adjusted_ksat_continue_from_1974_part_two.sh
@@ -5,7 +5,7 @@
 
 # transient run
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
-python parallel_pcrglobwb_with_prefactors_march_2016.py ../config/05min_runs_february_2016/with_modflow/adjusted_ksat/from_1950/continue_from_1974/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_from_1950_continue_from_1974_part_two.ini
+python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_february_2016/with_modflow/adjusted_ksat/from_1950/continue_from_1974/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_from_1950_continue_from_1974_part_two.ini
 
 # NOTE: pcrglobwb modflow with adjusted_ksat - start from 1950
 #                                              continue from 1974 (part_two)
