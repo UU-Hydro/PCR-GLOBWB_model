f2643af9369e944c8992f8d7c0fde3b7552b636a
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-04-04 12:01:05 +0200

    Execute/continue 05min_runs_february_2016/with_modflow/adjusted_ksat/from_1935/continue_from_1974/with_two_nodes/wmod1935_adjusted_ksat_continue_from_1974_part_one.sh and wmod1935_adjusted_ksat_continue_from_1974_part_two.sh.

---
+++ config/05min_runs_february_2016/with_modflow/adjusted_ksat/from_1935/continue_from_1974/with_two_nodes/wmod1935_adjusted_ksat_continue_from_1974_part_two.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal                                                                                                                                                                              
+
+# transient run
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_february_2016/with_modflow/adjusted_ksat/from_1935/continue_from_1974/with_two_nodes/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_from_1935_continue_from_1974_part_two.ini
+
+# NOTE: pcrglobwb modflow with adjusted_ksat - start from 1935
+#                                              continue from 1959
+#                                              continue from 1974 (part_two)
