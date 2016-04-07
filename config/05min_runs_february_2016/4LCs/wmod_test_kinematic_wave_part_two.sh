258a01a4aa798b3940842343f0dfa17bf22f41a1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-04-07 06:06:04 +0200

    Test using 4LCs with kinematic wave.

---
+++ config/05min_runs_february_2016/4LCs/wmod_test_kinematic_wave_part_two.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal                                                                                                                                                                              
+# transient run
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_february_2016/4LCs/setup_05min_pcrglobwb_modflow_cartesius_parallel_4LCs_edwin_parameter_set_test_part_two.ini
+
+# NOTE: pcrglobwb modflow with kinematic wave - test
