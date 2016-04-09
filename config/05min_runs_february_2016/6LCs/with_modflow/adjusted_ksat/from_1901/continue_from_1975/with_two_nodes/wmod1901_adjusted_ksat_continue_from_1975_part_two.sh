f1b8a0633c9d5c1549f6950f1f51ddef776b5e93
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-04-10 00:58:51 +0200

    Continue 05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901/continue_from_1975/

--- config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901/continue_from_1975/with_two_nodes/wmod1901_adjusted_ksat_continue_from_1975_part_two.sh
+++ config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901/continue_from_1975/with_two_nodes/wmod1901_adjusted_ksat_continue_from_1975_part_two.sh
@@ -5,7 +5,7 @@
 
 # transient run
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
-python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901/continue_from_1975/with_two_nodes/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_from_1901_continue_from_1975_part_one.ini
+python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901/continue_from_1975/with_two_nodes/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_from_1901_continue_from_1975_part_two.ini
 
 # NOTE: pcrglobwb modflow with adjusted_ksat - start from 1901
 #                                              continue from 1925
