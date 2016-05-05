1bf9410bee3d76711ea068517c80782469554900
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-05-05 21:02:39 +0200

    Test with short jobs.

--- config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901_spinup_test/begin/wmod1901_adjusted_ksat_spinup_test_part_two_SHORT.sh
+++ config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901_spinup_test/begin/wmod1901_adjusted_ksat_spinup_test_part_two_SHORT.sh
@@ -5,6 +5,6 @@
 
 # transient run
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
-python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901_spinup_test/begin/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_from_1901_begin_part_two.ini
+python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901_spinup_test/begin/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_from_1901_begin_part_two_SHORT.ini
 
 # NOTE: pcrglobwb modflow with adjusted_ksat - testing spin-up - part_two - start from 1901
