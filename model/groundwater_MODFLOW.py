2039560e11d6943fa5c0bc6661baedb766b8ffbe
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-05-11 16:51:20 +0200

    Include reporting.

--- model/groundwater_MODFLOW.py
+++ model/groundwater_MODFLOW.py
@@ -1043,7 +1043,7 @@ class GroundwaterModflow(object):
                     logger.warning(msg)
                     
                     print(self.criteria_HCLOSE)
-                    print(max(self.criteria_HCLOSE)
+                    print(max(self.criteria_HCLOSE))
                     
                     additional_HLCOSE = max(self.criteria_HCLOSE) + 0.5
                     self.criteria_HCLOSE.append(additional_HLCOSE)
