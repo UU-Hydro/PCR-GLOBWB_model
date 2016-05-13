d87a618d6faf13b0d80de73de8aa9449bdda7471
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-05-13 14:38:08 +0200

    Cleaning up files.

--- config/6LCs/with_modflow/adjusted_ksat/from_1901_extra_spin_up/steady-state/wmod1901_adjusted_ksat_extra_spinup_steady_state.sh
+++ config/6LCs/with_modflow/adjusted_ksat/from_1901_extra_spin_up/steady-state/wmod1901_adjusted_ksat_extra_spinup_steady_state.sh
@@ -4,7 +4,7 @@
 #SBATCH -p normal            
 
 # steady state run
-cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
-python deterministic_runner_for_monthly_merging_and_modflow_2016_03_29.py ../config/05min_runs_february_2016/6LCs/with_modflow/adjusted_ksat/from_1901_extra_spin_up/steady-state/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_extra_spinup.STEADYSTATE_natural_1901to1925.ini debug steady-state-only
+cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model/
+python deterministic_runner_for_monthly_merging_and_modflow_2016_03_29.py ../config/6LCs/with_modflow/adjusted_ksat/from_1901_extra_spin_up/steady-state/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_extra_spinup.STEADYSTATE_natural_1901to1925.ini
 
 # NOTE: pcrglobwb modflow with adjusted_ksat - steady-state
