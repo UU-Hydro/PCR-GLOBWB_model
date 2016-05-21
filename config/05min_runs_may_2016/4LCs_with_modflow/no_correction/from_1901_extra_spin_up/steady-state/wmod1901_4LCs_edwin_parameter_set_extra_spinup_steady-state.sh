54d27e5db321935674bfbf8618d27d9364d9ce1e
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-05-21 16:45:37 +0200

    Perform a steady-state run: wmod1901_4LCs_edwin_parameter_set_extra_spinup_steady-state.sh

--- config/05min_runs_may_2016/4LCs_with_modflow/no_correction/from_1901_extra_spin_up/steady-state/wmod1901_4LCs_edwin_parameter_set_extra_spinup_steady-state.sh
+++ config/05min_runs_may_2016/4LCs_with_modflow/no_correction/from_1901_extra_spin_up/steady-state/wmod1901_4LCs_edwin_parameter_set_extra_spinup_steady-state.sh
@@ -7,4 +7,4 @@
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model/
 python deterministic_runner_for_monthly_merging_and_modflow_2016_03_29.py ../config/05min_runs_may_2016/4LCs_with_modflow/adjusted_ksat/from_1901_extra_spin_up/steady-state/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_4LCs_edwin_parameter_set_extra_spinup.STEADYSTATE_natural_1901to1925.ini debug steady-state-only
 
-# NOTE: pcrglobwb modflow - 4LCs edwin parameter set - adjusted_ksat - natural - steady-state
+# NOTE: pcrglobwb modflow - 4LCs edwin parameter set - no_correction - natural - steady-state
