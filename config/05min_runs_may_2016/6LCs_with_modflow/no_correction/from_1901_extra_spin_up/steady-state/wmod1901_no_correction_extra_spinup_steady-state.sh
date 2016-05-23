c9aca3d862f6f16c9cd7a4b4333dd3a896da6d00
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-05-23 19:07:27 +0200

    Repairing wmod1901_no_correction_extra_spinup_steady-state.sh

--- config/05min_runs_may_2016/6LCs_with_modflow/no_correction/from_1901_extra_spin_up/steady-state/wmod1901_no_correction_extra_spinup_steady-state.sh
+++ config/05min_runs_may_2016/6LCs_with_modflow/no_correction/from_1901_extra_spin_up/steady-state/wmod1901_no_correction_extra_spinup_steady-state.sh
@@ -5,6 +5,6 @@
 
 # steady state run
 cd /home/edwin/github/edwinkost/PCR-GLOBWB/model/
-python deterministic_runner_for_monthly_merging_and_modflow_2016_03_29.py ../config/05min_runs_may_2016/6LCs_with_modflow/no_correction/from_1901_extra_spin_up/steady-state/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_no_correction_extra_spinup.STEADYSTATE_natural_1901to1925.ini
+python deterministic_runner_for_monthly_merging_and_modflow_2016_03_29.py ../config/05min_runs_may_2016/6LCs_with_modflow/no_correction/from_1901_extra_spin_up/steady-state/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_no_correction_extra_spinup.STEADYSTATE_natural_1901to1925.ini debug steady-state-only
 
 # NOTE: pcrglobwb modflow with 6LCs - no_correction - steady-state - natural
