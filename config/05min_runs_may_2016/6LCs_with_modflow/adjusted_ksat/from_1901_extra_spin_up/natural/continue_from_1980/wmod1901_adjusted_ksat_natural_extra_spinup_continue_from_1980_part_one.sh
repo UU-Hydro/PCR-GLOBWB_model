c386cb2d3e097eb5bf0f767c82629080be9f0ba1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-05-25 23:46:49 +0200

    Preparing config/05min_runs_may_2016/6LCs_with_modflow/adjusted_ksat/from_1901_extra_spin_up/natural/continue_from_1980/

---
+++ config/05min_runs_may_2016/6LCs_with_modflow/adjusted_ksat/from_1901_extra_spin_up/natural/continue_from_1980/wmod1901_adjusted_ksat_natural_extra_spinup_continue_from_1980_part_one.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal
+
+# transient run
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_may_2016/6LCs_with_modflow/adjusted_ksat/from_1901_extra_spin_up/natural/continue_from_1980/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_6LCs_original_parameter_set_adjusted_ksat_natural_extra_spinup_from_1901_continue_from_1980_part_one.ini
+
+# NOTE: pcrglobwb modflow with adjusted_ksat - with extra spin-up - natural - part_one - start from 1901
+#                                                                                        continue from 1943
+#                                                                                        continue from 1980
