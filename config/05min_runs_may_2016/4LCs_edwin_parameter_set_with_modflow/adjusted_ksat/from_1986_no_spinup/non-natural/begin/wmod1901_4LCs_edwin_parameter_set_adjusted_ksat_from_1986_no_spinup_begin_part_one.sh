698c09a6bff11c2e1ea66644552eefdd95807f92
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-06-08 23:27:53 +0200

    Prepare config/05min_runs_may_2016/4LCs_edwin_parameter_set_with_modflow/adjusted_ksat/from_1986_no_spinup/non-natural/begin

---
+++ config/05min_runs_may_2016/4LCs_edwin_parameter_set_with_modflow/adjusted_ksat/from_1986_no_spinup/non-natural/begin/wmod1901_4LCs_edwin_parameter_set_adjusted_ksat_from_1986_no_spinup_begin_part_one.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal
+
+# transient run
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_with_prefactors_2016_03_29.py ../config/05min_runs_may_2016/4LCs_edwin_parameter_set_with_modflow/adjusted_ksat/from_1986_no_spinup/non-natural/begin/setup_05min_CRU-TS3.23_ERA20C_pcrglobwb_with_modflow_4LCs_edwin_parameter_set_adjusted_ksat_from_1986_no_spinup_begin_part_one.ini
+
+# NOTE: pcrglobwb modflow - 4 LCs - edwin parameter set - adjusted_ksat - non-natural - from 1986 no spinup - part_one - start from 1986
+
