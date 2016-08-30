2057a88e1b2f67e617cb7adcefffe1a6dd4b6e00
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-08-30 22:33:42 +0200

    Start a spin-up run for natural run (2/4 land covers) with WATCH.

--- model/deterministic_runner_glue_with_parallel_and_modflow_options.py
+++ model/deterministic_runner_glue_with_parallel_and_modflow_options.py
@@ -272,12 +272,6 @@ class DeterministicRunner(DynamicModel):
         # do any needed reporting for this time step        
         self.reporting.report()
 
-        print self.modelTime.isLastDayOfMonth()
-        print self.configuration.online_coupling_between_pcrglobwb_and_modflow
-        print self.with_merging
-        print self.configuration.globalOptions.keys()
-        print self.configuration.globalOptions["with_merging"]
-        
         # at the last day of the month, stop calculation until modflow and related merging process are ready (only for a run with modflow) 
         if self.modelTime.isLastDayOfMonth() and (self.configuration.online_coupling_between_pcrglobwb_and_modflow or\
                                                   self.with_merging):
