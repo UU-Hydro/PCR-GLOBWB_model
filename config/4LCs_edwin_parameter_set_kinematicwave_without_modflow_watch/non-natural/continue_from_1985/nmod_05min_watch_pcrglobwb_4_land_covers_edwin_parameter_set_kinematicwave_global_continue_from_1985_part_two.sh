68422b28c4f75c2def68aef79306cbef396d77f1
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-09-13 16:12:21 +0200

    Continue 5min_watch_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_global_continue_from_1985

---
+++ config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_watch/non-natural/continue_from_1985/nmod_05min_watch_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_global_continue_from_1985_part_two.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal
+
+cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_runner.py ../config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_watch/non-natural/continue_from_1985/setup_05min_watch_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_global_continue_from_1985_part_two.ini
+
