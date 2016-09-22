b1c7daa01234c6e9f0a5073d8b8bdfa462f1bb25
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-09-22 14:06:30 +0200

    Start 5min_noresm1-m_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_begin_since_1951

---
+++ config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_noresm1-m/non-natural/begin_since_1951/nmod_05min_noresm1-m_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_begin_since_1951_part_two.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 98:59:00 
+#SBATCH -p normal
+
+# load a special version of pcraster that is optimized for pcraster-modflow (provided by Oliver)
+. /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
+
+# run using the account of "edwin":
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_runner.py ../config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_noresm1-m/non-natural/begin_since_1951/setup_05min_noresm1-m_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_begin_since_1951_part_two.ini
