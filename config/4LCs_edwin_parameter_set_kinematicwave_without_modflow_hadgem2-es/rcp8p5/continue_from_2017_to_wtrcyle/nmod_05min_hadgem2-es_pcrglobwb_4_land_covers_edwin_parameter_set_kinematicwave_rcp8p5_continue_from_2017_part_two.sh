780ef57feb31090414d0d8a09a6b8b09b772a978
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-10-10 14:13:06 +0200

    Continue 05min_hadgem2-es_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp8p5_continue_from_2017

--- config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_hadgem2-es/rcp8p5/continue_from_2017_to_wtrcyle/nmod_05min_hadgem2-es_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp8p5_continue_from_2017_part_two.sh
+++ config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_hadgem2-es/rcp8p5/continue_from_2017_to_wtrcyle/nmod_05min_hadgem2-es_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp8p5_continue_from_2017_part_two.sh
@@ -7,5 +7,5 @@
 . /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
 
 
-cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
 python parallel_pcrglobwb_runner.py ../config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_hadgem2-es/rcp8p5/continue_from_2017_to_wtrcyle/setup_05min_hadgem2-es_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp8p5_continue_from_2017_part_two.ini
