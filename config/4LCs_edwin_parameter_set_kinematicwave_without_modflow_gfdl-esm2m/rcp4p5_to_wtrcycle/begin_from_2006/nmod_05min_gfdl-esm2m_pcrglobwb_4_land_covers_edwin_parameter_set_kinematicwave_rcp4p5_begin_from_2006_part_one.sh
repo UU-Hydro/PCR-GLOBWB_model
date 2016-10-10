3d6ad459dbbad4c1226957bbc9459d6442c417c6
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-10-10 13:07:40 +0200

    Start 05min_gfdl-esm2m_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp4p5_begin_from_2006

--- config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_gfdl-esm2m/rcp4p5_to_wtrcycle/begin_from_2006/nmod_05min_gfdl-esm2m_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp4p5_begin_from_2006_part_one.sh
+++ config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_gfdl-esm2m/rcp4p5_to_wtrcycle/begin_from_2006/nmod_05min_gfdl-esm2m_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp4p5_begin_from_2006_part_one.sh
@@ -7,5 +7,5 @@
 . /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
 
 
-cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
+cd /home/edwin/github/edwinkost/PCR-GLOBWB/model
 python parallel_pcrglobwb_runner.py ../config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_gfdl-esm2m/rcp4p5_to_wtrcycle/begin_from_2006/setup_05min_gfdl-esm2m_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_rcp4p5_begin_from_2006_part_one.ini
