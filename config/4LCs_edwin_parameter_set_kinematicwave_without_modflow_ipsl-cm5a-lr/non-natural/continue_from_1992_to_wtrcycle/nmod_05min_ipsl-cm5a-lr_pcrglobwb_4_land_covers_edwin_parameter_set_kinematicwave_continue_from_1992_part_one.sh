0d25e18cebbe0e92af3aa17562985203d4ec2232
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-10-10 15:36:34 +0200

    Continue 05min_ipsl-cm5a-lr_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_continue_from_1992

--- config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_ipsl-cm5a-lr/non-natural/continue_from_1992_to_wtrcycle/nmod_05min_ipsl-cm5a-lr_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_continue_from_1992_part_one.sh
+++ config/4LCs_edwin_parameter_set_kinematicwave_without_modflow_ipsl-cm5a-lr/non-natural/continue_from_1992_to_wtrcycle/nmod_05min_ipsl-cm5a-lr_pcrglobwb_4_land_covers_edwin_parameter_set_kinematicwave_continue_from_1992_part_one.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 119:59:00 
+#SBATCH -t 116:59:00 
 #SBATCH -p normal
 
 # load a special version of pcraster that is optimized for pcraster-modflow (provided by Oliver)
