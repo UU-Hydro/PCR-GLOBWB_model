86bffd20a01556a63737954bcfb43c45886c0b0b
Edwin Sutanudjaja <Edwin.Sutanudjaja@deltares.nl>
2016-10-24 22:15:01 +0200

    Start 05min_hadgem2-es_continue_from_1955

--- config/05min_runs_2016_october/4LCs_hadgem2-es/non-natural/continue_from_1955/nmod_05min_hadgem2-es_continue_from_1955_part_two.sh
+++ config/05min_runs_2016_october/4LCs_hadgem2-es/non-natural/continue_from_1955/nmod_05min_hadgem2-es_continue_from_1955_part_two.sh
@@ -7,4 +7,4 @@
 . /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
 
 cd /home/edwinsut/github/edwinkost/PCR-GLOBWB/model
-python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_hadgem2-es/non-natural/continue_from_1955/setup_05min_hadgem2-es_continue_from_1960_part_two.ini
+python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_hadgem2-es/non-natural/continue_from_1955/setup_05min_hadgem2-es_continue_from_1955_part_two.ini
