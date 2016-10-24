1300d9ad126662e22052654f412459c42433c7f1
Edwin Sutanudjaja <Edwin.Sutanudjaja@deltares.nl>
2016-10-24 22:54:28 +0200

    Start _05min_gfdl-esm2m_continue_from_1958

---
+++ config/05min_runs_2016_october/4LCs_gfdl-esm2m/non-natural/continue_from_1958/nmod_05min_gfdl-esm2m_continue_from_1958_part_one.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal
+
+# load a special version of pcraster
+. /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
+
+cd /home/edwinsut/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_gfdl-esm2m/non-natural/continue_from_1958/setup_05min_gfdl-esm2m_continue_from_1958_part_one.ini
