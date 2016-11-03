94eac3f6d55c0cfbeb73c9f31c904e4c6a3f5282
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-11-03 21:12:57 +0100

    Start 05min_runs_2016_october/4LCs_hadgem2-es/rcp4p5

--- config/05min_runs_2016_october/4LCs_hadgem2-es/rcp4p5/begin_from_2006/nmod_05min_hadgem2-es_rcp4p5_begin_from_2006_part_one.sh
+++ config/05min_runs_2016_october/4LCs_hadgem2-es/rcp4p5/begin_from_2006/nmod_05min_hadgem2-es_rcp4p5_begin_from_2006_part_one.sh
@@ -6,5 +6,5 @@
 # load a special version of pcraster
 . /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
 
-cd /home/edwinsut/github/edwinkost/PCR-GLOBWB/model
+cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
 python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_hadgem2-es/rcp4p5/begin_from_2006/setup_05min_hadgem2-es_rcp4p5_begin_from_2006_part_one.ini
