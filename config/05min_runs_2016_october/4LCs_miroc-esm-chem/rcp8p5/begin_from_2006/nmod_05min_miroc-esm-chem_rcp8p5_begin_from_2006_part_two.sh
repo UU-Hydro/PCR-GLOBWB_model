27f4f0c37b35cdd2d7f1d23094dfb8244c4a6307
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-11-03 08:08:48 +0100

    Start config/05min_runs_2016_october/4LCs_miroc-esm-chem/rcp8p5/

---
+++ config/05min_runs_2016_october/4LCs_miroc-esm-chem/rcp8p5/begin_from_2006/nmod_05min_miroc-esm-chem_rcp8p5_begin_from_2006_part_two.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 94:45:00 
+#SBATCH -p normal
+
+# load a special version of pcraster
+. /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
+
+cd /home/edwinsut/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_miroc-esm-chem/rcp8p5/begin_from_2006/setup_05min_miroc-esm-chem_rcp8p5_begin_from_2006_part_two.ini
