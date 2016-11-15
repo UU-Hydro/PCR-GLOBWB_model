1f9eb546154a4bf608218cc2dee13efb8c83495a
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-11-15 17:00:19 +0100

    Continue/start 4LCs_miroc-esm-chem/rcp4p5/continue_from_2031

---
+++ config/05min_runs_2016_october/4LCs_miroc-esm-chem/rcp4p5/continue_from_2031/nmod_05min_miroc-esm-chem_rcp4p5_continue_from_2031_part_two.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal
+
+# load a special version of pcraster
+. /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
+
+cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_miroc-esm-chem/rcp4p5/continue_from_2031/setup_05min_miroc-esm-chem_rcp4p5_continue_from_2031_part_two.ini
