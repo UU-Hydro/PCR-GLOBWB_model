d010d7d49166591476e9b9e311d2a7d98e4de83a
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-13 22:57:37 +0100

    start/continue 05min_hadgem2-es_rcp8p5_continue_from_2055

---
+++ config/05min_runs_2016_october/4LCs_hadgem2-es/rcp8p5/continue_from_2055/nmod_05min_hadgem2-es_rcp8p5_continue_from_2055_part_two.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal
+
+# load a special version of pcraster
+. /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
+
+cd /home/edwinsut/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_hadgem2-es/rcp8p5/continue_from_2055/setup_05min_hadgem2-es_rcp8p5_continue_from_2055_part_two.ini
