a62d202aeb07ff98092b6c947d4e0650fb1ca153
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-13 16:39:15 +0100

    Start/continue 05min_runs_2016_october/4LCs_gfdl-esm2m/rcp4p5/continue_from_2055/setup_05min_gfdl-esm2m_rcp4p5_continue_from_2055_part_one.ini

--- config/05min_runs_2016_october/4LCs_gfdl-esm2m/rcp4p5/continue_from_2055/nmod_05min_gfdl-esm2m_rcp4p5_continue_from_2055_part_two.sh
+++ config/05min_runs_2016_october/4LCs_gfdl-esm2m/rcp4p5/continue_from_2055/nmod_05min_gfdl-esm2m_rcp4p5_continue_from_2055_part_two.sh
@@ -2,6 +2,7 @@
 #SBATCH -N 1
 #SBATCH -t 119:59:00 
 #SBATCH -p normal
+#SBATCH --constraint=haswell
 
 # load a special version of pcraster
 . /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
