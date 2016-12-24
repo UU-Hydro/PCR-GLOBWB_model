8baedefd18e4a2ae5616af9e44e571001e0e039b
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-24 04:53:35 +0100

    start/continue 05min_gfdl-esm2m_rcp4p5_continue_from_2080

--- config/05min_runs_2016_october/4LCs_gfdl-esm2m/rcp4p5/continue_from_2080/nmod_05min_gfdl-esm2m_rcp4p5_continue_from_2080_part_two.sh
+++ config/05min_runs_2016_october/4LCs_gfdl-esm2m/rcp4p5/continue_from_2080/nmod_05min_gfdl-esm2m_rcp4p5_continue_from_2080_part_two.sh
@@ -7,4 +7,4 @@
 . /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
 
 cd /home/edwinsut/github/edwinkost/PCR-GLOBWB/model
-python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_gfdl-esm2m/rcp4p5/continue_from_2080/setup_05min_gfdl-esm2m_rcp4p5_continue_from_2080_part_one.ini
+python parallel_pcrglobwb_runner.py ../config/05min_runs_2016_october/4LCs_gfdl-esm2m/rcp4p5/continue_from_2080/setup_05min_gfdl-esm2m_rcp4p5_continue_from_2080_part_two.ini
