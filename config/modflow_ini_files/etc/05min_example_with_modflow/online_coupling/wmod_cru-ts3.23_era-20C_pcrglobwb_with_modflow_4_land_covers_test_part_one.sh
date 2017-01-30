a315f8db62ab5e9be7e1895730e0b7f90832d757
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-01-30 09:43:11 +0100

    In progress.

---
+++ config/modflow_ini_files/etc/05min_example_with_modflow/online_coupling/wmod_cru-ts3.23_era-20C_pcrglobwb_with_modflow_4_land_covers_test_part_one.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 59:00 
+#SBATCH -p normal
+
+# load a special version of pcraster
+. /home/edwin/bin-special/pcraster-4.1.0-beta-20151027_x86-64_gcc-4/bashrc_special_pcraster_modflow
+
+cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model
+python parallel_pcrglobwb_runner.py ../config/05min_example_with_modflow/online_coupling/setup_05min_cru-ts3.23_era-20C_pcrglobwb_with_modflow_4_land_covers_test_part_one.ini
