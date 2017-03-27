3bee1bcc6a2b7c2f5b6f38227316da349ed2cfd5
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-03-27 13:54:49 +0200

    test again

--- model/virtualOS.py
+++ model/virtualOS.py
@@ -1517,7 +1517,7 @@ def waterAbstractionAndAllocation(water_demand_volume,available_water_volume,all
     cellAvlWater  = pcr.min(cellAvlWater, zoneVolDemand)
     
     # avoid small values
-    cellAvlWater  = pcr.ifthenelse(cellAvlWater > pcr.areaaverage(cellAvlWater, allocation_zones), cellAvlWater, 0.0)
+    cellAvlWater  = pcr.ifthenelse(cellAvlWater > pcr.areaaverage(pcr.ifthen(cellAvlWater > 0.0, cellAvlWater), allocation_zones), cellAvlWater, 0.0)
     
     # total available water volume in each zone/segment (unit: m3)
     # - to minimize numerical errors, separating cellAvlWater 
