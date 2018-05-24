e4c834515bda67574fc1d603dc0ab4e4b5795043
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-05-24 14:36:44 +0200

    Getting river package input for Daniel based on the GMD 5min run.

--- model/groundwater_MODFLOW.py
+++ model/groundwater_MODFLOW.py
@@ -1787,6 +1787,10 @@ class GroundwaterModflow(object):
         # set the RIV package only to the uppermost layer
         self.pcr_modflow.setRiver(surface_water_elevation, self.surface_water_bed_elevation, bed_conductance_used, self.number_of_layers)
         
+        pcr.report(surface_water_elevation, "riv_head_surface_water_elevation.map")
+        pcr.report(self.surface_water_bed_elevation, "riv_bottom_surface_water_bed_elevation.map")
+        pcr.report(bed_conductance_used, "riv_bed_conductance.map")
+        
         # TODO: Improve the concept of RIV package, particularly while calculating surface water elevation in lakes and reservoirs
 
         # set drain package
