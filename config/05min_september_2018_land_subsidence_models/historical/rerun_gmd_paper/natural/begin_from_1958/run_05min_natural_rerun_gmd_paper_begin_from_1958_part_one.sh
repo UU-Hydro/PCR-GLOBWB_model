2dae3bb5fc5c49ce82c4f6cb4f13dff55f5bf1a4
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-10-01 09:23:31 +0200

    Try to run it again using the previous version of pcraster.

--- config/05min_september_2018_land_subsidence_models/historical/rerun_gmd_paper/natural/begin_from_1958/run_05min_natural_rerun_gmd_paper_begin_from_1958_part_one.sh
+++ config/05min_september_2018_land_subsidence_models/historical/rerun_gmd_paper/natural/begin_from_1958/run_05min_natural_rerun_gmd_paper_begin_from_1958_part_one.sh
@@ -4,7 +4,8 @@
 #SBATCH -p normal
 
 # go to the PCR-GLOBWB model folder
-cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model/
+cd /home/edwinsut/github/edwinkost/PCR-GLOBWB/model/
 
 # then execute your PCR-GLOBWB model run
 python parallel_pcrglobwb_runner.py ../config/05min_september_2018_land_subsidence_models/historical/rerun_gmd_paper/natural/begin_from_1958/setup_05min_natural_rerun_gmd_paper_begin_from_1958_part_one.ini
+
