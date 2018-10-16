db3e538649b43084411d63968e176d32a201cffb
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-10-16 15:27:37 +0200

    Rerun again with the correct soil properties.

---
+++ config/05min_october_2018_land_subsidence_models/historical/with_7_land_covers/natural_only_spinup_correct_soil/begin_from_1955/run_05min_natural_spinup_correct_soil_begin_from_1955_part_two.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00 
+#SBATCH -p normal
+
+# go to the PCR-GLOBWB model folder
+cd /home/edwinhs/github/edwinkost/PCR-GLOBWB/model/
+
+# then execute your PCR-GLOBWB model run
+python parallel_pcrglobwb_runner.py ../config/05min_october_2018_land_subsidence_models/historical/with_7_land_covers/natural_only_spinup_correct_soil/begin_from_1955/setup_05min_natural_spinup_begin_from_1955_part_two.ini
+
