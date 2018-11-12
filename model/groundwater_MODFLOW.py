349a6b31f01badf2306055fa883db01cb7f20b3a
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-11-12 20:30:03 +0100

    Setup and test LAYCON 02

--- model/groundwater_MODFLOW.py
+++ model/groundwater_MODFLOW.py
@@ -880,6 +880,7 @@ class GroundwaterModflow(object):
         if self.log_to_info: logger.info(msg)
         self.pcr_modflow.setConductivity(00, horizontal_conductivity_layer_2, \
                                              vertical_conductivity_layer_2, 2)              
+        dummy
         if "aquiferLayerSecondaryStorageCoefficient" in self.iniItems.modflowParameterOptions.keys() and\
             self.iniItems.modflowParameterOptions['aquiferLayerSecondaryStorageCoefficient'] not in ["None", "False"]:
             msg = "Using the layer type (LAYCON) 2 for the aquifer layer."
