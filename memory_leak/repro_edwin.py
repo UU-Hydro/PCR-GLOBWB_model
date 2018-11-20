20c5b232a5e8cb9a9a90fb4d3863bd5d0e4d4e1f
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-11-20 15:47:33 +0100

    Try to show memory leaks

--- memory_leak/repro_edwin.py
+++ memory_leak/repro_edwin.py
@@ -13,22 +13,22 @@ log_file = file("memory.col", "w")
 
 
 def print_memory_usage():
-    nr_mbytes = process.get_memory_info()[0] / 1048576.0
+    nr_mbytes = process.virtual_memory()[0] / 1048576.0
     log_file.write("{}\n".format(nr_mbytes))
     sys.stdout.write("{}\n".format(nr_mbytes))
     sys.stdout.flush()
 
 
 
-#~ print_memory_usage()
+print_memory_usage()
 
 counter = 0
-#~ for i in xrange(100):
-while True: 
+for i in xrange(100):
+#~ while True: 
     counter = counter + 1
     print counter
     test_map = pcr.readmap("dummy.map")
     test_num = pcr.pcr2numpy(test_map, 0)[:]
-    #~ print_memory_usage()
+    print_memory_usage()
 
 log_file.close()
