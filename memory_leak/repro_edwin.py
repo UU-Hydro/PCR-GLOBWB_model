37c4ccb0563a103e796d5eed06d92ff4cc2d9953
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-11-20 15:38:02 +0100

    Try to show memory leaks

--- memory_leak/repro_edwin.py
+++ memory_leak/repro_edwin.py
@@ -3,23 +3,22 @@
 
 import os
 import sys
-
+import psutil
 #~ import netCDF4
 import numpy as np
 import pcraster as pcr
 
+process = psutil.Process(os.getpid())
+log_file = file("memory.col", "w")
+
 
 def print_memory_usage():
-    import psutil
-    p = psutil.Process(os.getpid())
-    print p  
-    nr_mbytes = p.get_memory_info()[0] / 1048576.0
+    nr_mbytes = process.checked_memory_info()[0] / 1048576.0
     log_file.write("{}\n".format(nr_mbytes))
     sys.stdout.write("{}\n".format(nr_mbytes))
     sys.stdout.flush()
 
 
-log_file = file("memory.col", "w")
 
 print_memory_usage()
 
@@ -28,6 +27,6 @@ for i in xrange(100):
     test_map = pcr.readmap("dummy.map")
     test_num = pcr.pcr2numpy(test_map, 0)
     result = variable * numpy.ones_like(variable)
-    print_memory_usage()
+    #~ print_memory_usage()
 
 log_file.close()
