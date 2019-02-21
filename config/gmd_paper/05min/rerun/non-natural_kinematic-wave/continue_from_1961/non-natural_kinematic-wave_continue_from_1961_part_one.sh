1797dcc3c3fb28716a395ba4445245c16bffc279
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-02-21 17:57:35 +0100

    non-natural_kinematic-wave_continue_from_1961

--- config/gmd_paper/05min/rerun/non-natural_kinematic-wave/continue_from_1961/non-natural_kinematic-wave_continue_from_1961_part_one.sh
+++ config/gmd_paper/05min/rerun/non-natural_kinematic-wave/continue_from_1961/non-natural_kinematic-wave_continue_from_1961_part_one.sh
@@ -2,8 +2,8 @@
 #SBATCH -N 1
 #SBATCH -t 119:59:00 
 #SBATCH -p normal
-#SBATCH --constraint=haswell                                                                                                                          
-#SBATCH -J non-natural_kinematic-wave_continue_from_1961_part_one
+#SBATCH --constraint=haswell
+#SBATCH -J non-natural_kinematic-wave_continue_from_1961_part_two
 
 # mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
--- config/gmd_paper/05min/rerun/non-natural_kinematic-wave/continue_from_1961/non-natural_kinematic-wave_continue_from_1961_part_two.sh
+++ config/gmd_paper/05min/rerun/non-natural_kinematic-wave/continue_from_1961/non-natural_kinematic-wave_continue_from_1961_part_two.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1
 #SBATCH -t 119:59:00 
 #SBATCH -p normal
-#SBATCH --constraint=haswell                                                                                                                          
+#SBATCH --constraint=haswell
 #SBATCH -J non-natural_kinematic-wave_continue_from_1961_part_two
 
 # mail alert at start, end and abortion of execution
