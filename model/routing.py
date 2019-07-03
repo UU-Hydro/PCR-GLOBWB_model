0e0fc43fdb6d750a2033d381be2a8a4ac6e180ee
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-07-03 02:48:04 +0200

    test_with_python3_and_new_pcraster

--- model/routing.py
+++ model/routing.py
@@ -21,10 +21,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import types
 import math
 import types
+import itertools
 
 from six.moves import map
 
