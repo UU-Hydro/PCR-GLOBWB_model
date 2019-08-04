2521633bb5fa7b430707a66658b1f3198635eaa5
U-DESKTOP-92P3V7F\rikat <rika_small_laptop@mobaxterm>
2019-08-04 02:05:18 +0200

    start test run on eejit for land subsidence model

--- model/disclaimer.py
+++ model/disclaimer.py
@@ -3,7 +3,6 @@
 
 # encoding=utf8
 import sys
-sys.setdefaultencoding('utf8')
 
 import logging
 logger = logging.getLogger(__name__)
@@ -17,7 +16,7 @@ def print_disclaimer(with_logger = False):
     disclaimer_message +=                                                                                                  "\n"
     disclaimer_message += " Copyright (C) 2016, Edwin H. Sutanudjaja, Rens van Beek, Niko Wanders, Yoshihide Wada,     " + "\n"
     disclaimer_message += " Joyce H. C. Bosmans, Niels Drost, Ruud J. van der Ent, Inge E. M. de Graaf, Jannis M. Hoch," + "\n"
-    # ~ disclaimer_message += " Kor de Jong, Derek Karssenberg, Patricia López López, Stefanie Peßenteiner, Oliver Schmitz," + "\n"
+    disclaimer_message += " Kor de Jong, Derek Karssenberg, Patricia López López, Stefanie Peßenteiner, Oliver Schmitz," + "\n"
     disclaimer_message += " Menno W. Straatsma, Ekkamol Vannametee, Dominik Wisser, and Marc F. P. Bierkens            " + "\n"
     disclaimer_message += " Faculty of Geosciences, Utrecht University, Utrecht, The Netherlands                       " + "\n"
     disclaimer_message +=                                                                                                  "\n"
