357b9698f5d36464bba54dc21d8bc81c36fbc6c4
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-08-14 14:10:55 +0200

    The scripts should be ready, we just have to execute them on eejit

--- config/test_with_pcraster_4.3.0/main_job_list.sh
+++ config/test_with_pcraster_4.3.0/main_job_list.sh
@@ -3,6 +3,7 @@
 set -x
 
 # set a working folder on a scratch disk (it will contains slurm output)
+# - for tests on cartesius
 WORK_FOLDER="/scratch-shared/edwin/pcrglobwb_pcraster4.3_test_on_eejit"
 mkdir -p ${WORK_FOLDER}
 
@@ -13,13 +14,13 @@ cp run_05min_test_on_eejit_natural_test_on_cartesius.sh ${WORK_FOLDER}
 cd ${WORK_FOLDER}
 
 # submit the jobs
-#~ sbatch --export=NUMBER_OF_WORKING_THREADS=-1 -J pcrt_-1 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-#~ sbatch --export=NUMBER_OF_WORKING_THREADS=0  -J pcrt_0  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-#~ sbatch --export=NUMBER_OF_WORKING_THREADS=1  -J pcrt_1  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-#~ sbatch --export=NUMBER_OF_WORKING_THREADS=2  -J pcrt_2  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-#~ sbatch --export=NUMBER_OF_WORKING_THREADS=4  -J pcrt_4  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-#~ sbatch --export=NUMBER_OF_WORKING_THREADS=8  -J pcrt_8  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-#~ sbatch --export=NUMBER_OF_WORKING_THREADS=16 -J pcrt_16 run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=-1 -J pcrt_-1 run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=0  -J pcrt_0  run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=1  -J pcrt_1  run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=2  -J pcrt_2  run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=4  -J pcrt_4  run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=8  -J pcrt_8  run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=16 -J pcrt_16 run_05min_test_on_eejit_natural_test_on_cartesius.sh
 sbatch --export=NUMBER_OF_WORKING_THREADS=24 -J pcrt_24 run_05min_test_on_eejit_natural_test_on_cartesius.sh
 sbatch --export=NUMBER_OF_WORKING_THREADS=32 -J pcrt_32 run_05min_test_on_eejit_natural_test_on_cartesius.sh
 sbatch --export=NUMBER_OF_WORKING_THREADS=48 -J pcrt_48 run_05min_test_on_eejit_natural_test_on_cartesius.sh
