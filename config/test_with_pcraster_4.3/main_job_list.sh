08e8f7c0944a51d9836d0d501923a1118a90359d
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-08-15 13:53:44 +0200

    start testing on cartesius

--- config/test_with_pcraster_4.3/main_job_list.sh
+++ config/test_with_pcraster_4.3/main_job_list.sh
@@ -4,29 +4,32 @@ set -x
 
 # set a working folder on a scratch disk (it will contains slurm output)
 # - for tests on cartesius
-WORK_FOLDER="/scratch-shared/edwin/pcrglobwb_pcraster4.3_test_on_eejit"
+WORK_FOLDER="/scratch/depfg/sutan101/pcrglobwb_pcraster4.3_test_on_eejit"
 mkdir -p ${WORK_FOLDER}
 
+# master job file
+JOB_FILE=run_05min_test_on_eejit_natural.sh
+
 # copy the master job file to the working folder
-cp run_05min_test_on_eejit_natural_test_on_cartesius.sh ${WORK_FOLDER}
+cp ${JOB_FILE} ${WORK_FOLDER}
 
 # go to the working folder
 cd ${WORK_FOLDER}
 
 # submit the jobs
-sbatch --export=NUMBER_OF_WORKING_THREADS=-1 -J pcrt_-1 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=0  -J pcrt_0  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=1  -J pcrt_1  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=2  -J pcrt_2  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=4  -J pcrt_4  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=8  -J pcrt_8  run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=16 -J pcrt_16 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=24 -J pcrt_24 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=32 -J pcrt_32 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=48 -J pcrt_48 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=64 -J pcrt_64 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=80 -J pcrt_80 run_05min_test_on_eejit_natural_test_on_cartesius.sh
-sbatch --export=NUMBER_OF_WORKING_THREADS=96 -J pcrt_96 run_05min_test_on_eejit_natural_test_on_cartesius.sh
+sbatch --export=NUMBER_OF_WORKING_THREADS=-1 -J pcrt_c-1 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=0  -J pcrt_c00 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=1  -J pcrt_c01 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=2  -J pcrt_c02 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=4  -J pcrt_c04 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=8  -J pcrt_c08 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=16 -J pcrt_c16 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=24 -J pcrt_c24 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=32 -J pcrt_c32 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=48 -J pcrt_c48 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=64 -J pcrt_c64 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=80 -J pcrt_c80 ${JOB_FILE}
+sbatch --export=NUMBER_OF_WORKING_THREADS=96 -J pcrt_c96 ${JOB_FILE}
 
 # go to the previous folder
 cd -
