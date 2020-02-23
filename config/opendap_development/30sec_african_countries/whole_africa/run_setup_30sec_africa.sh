3f93fca72582f33b5ba612518e910928eb9bab75
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-02-23 07:32:57 +0100

    fixing support to multicore; test with a small test

---
+++ config/opendap_development/30sec_african_countries/whole_africa/run_setup_30sec_africa.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 96
+#~ #SBATCH -t 240:00:00
+#SBATCH -p defq
+#SBATCH -J 30sec_africa_version_20200218
+
+#SBATCH --exclusive
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=edwinkost@gmail.com
+
+conda activate pcrglobwb_py3_env_v20200128
+
+pcrcalc
+
+export PCRASTER_NR_WORKER_THREADS=36
+
+cd /quanta1/home/sutan101/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model/
+
+python deterministic_runner.py ..config/opendap_development/30sec_african_countries/whole_africa/setup_30sec_africa.ini
+
+pcrcalc
+
+
+
