14849199673f5c5ac7c514ca3fee9df2ff00ed73
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-03-31 09:05:44 +0200

    continue testing

--- model/meteo.py
+++ model/meteo.py
@@ -106,7 +106,7 @@ class Meteo(object):
 
         # latitudes (required for the Hamon and Penman-Monteith method)
         self.latitudes = pcr.ycoordinate(self.cloneMap) # needed to calculate 'referenceETPot'
-        self.latitudes_in_radian = penman_monteith.shortwave_radiation.deg2rad(self.latitudes)    
+        self.latitudes_in_radian = vos.deg2rad(self.latitudes)    
         
         # initiate shortwave radiation class, required for the Bristow-Campbell method
         self.sw_rad_based_on_bristow_campbell = False
