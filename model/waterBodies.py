cf2fb7b6d2cc938483ebec366cd1e6f13657ea04
U-SOLISCOM\Sutan101 <Sutan101@UU078667.soliscom.uu.nl>
2020-06-10 14:17:52 +0200

    çonvert example forcing to nc3

--- model/waterBodies.py
+++ model/waterBodies.py
@@ -280,8 +280,8 @@ class WaterBodies(object):
                     self.resMaxCapInp+str(year_used)+".map", \
                     self.cloneMap,self.tmpDir,self.inputDir)
 
-        self.resMaxCap = pcr.ifthenelse(self.resMaxCap > 0.,\
-                                        self.resMaxCap, 0.0)
+        self.resMaxCap = pcr.ifthen(self.resMaxCap > 0.,\
+                                    self.resMaxCap)
         self.resMaxCap = pcr.areaaverage(self.resMaxCap,\
                                          self.waterBodyIds)
                                          
