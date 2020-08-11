be96386df2873d7be744d5a2687e0cb83126d486
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-11 23:55:25 +0200

    test ini file

--- model/deterministic_runner_parallel_for_ulysses.py
+++ model/deterministic_runner_parallel_for_ulysses.py
@@ -425,7 +425,7 @@ def modify_ini_file(original_ini_file,
     # - for a run that is part of a set of parallel (clone) runs
     if system_argument[2] == "parallel" or system_argument[2] == "debug_parallel" or system_argument[2] == "debug-parallel":
         clone_code = str(system_argument[3])
-        output_folder_with_clone_code = "M%02i" %int(clone_code)
+        output_folder_with_clone_code = "M%07i" %int(clone_code)
         folder_for_ini_files = os.path.join(main_output_dir, output_folder_with_clone_code, "ini_files") 
     
    # create folder
@@ -481,7 +481,7 @@ def main():
         # - clone code in string
         clone_code = str(sys.argv[3])
         # - output folder
-        output_folder_with_clone_code = "M%2i" %int(clone_code)
+        output_folder_with_clone_code = "M%07i" %int(clone_code)
         configuration.globalOptions['outputDir'] += output_folder_with_clone_code 
         # - clone map
         configuration.globalOptions['cloneMap']   = configuration.globalOptions['cloneMap'] %(clone_code)
