e82556f95ef9684dd25a54511ec7ea6ef21dbeb7
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-12 01:12:14 +0200

    test ini file

--- model/example_bash.sh
+++ model/example_bash.sh
@@ -5,7 +5,7 @@ git pull; python deterministic_runner_parallel_for_ulysses.py \
           -sd 1982-01-01 -ed 1982-01-31 \
           -misd /scratch/ms/copext/cyes/pcrglobwb_output_version_2020-10-08_example/first_test_54_clones/global/states \
           -dfis 1981-12-31 \
-          -pff    /scratch/mo/nest/ulysses/data/meteo/era5land/1982/01/precipitation_daily_01_1981.nc \
-          -tff    /scratch/mo/nest/ulysses/data/meteo/era5land/1982/01/tavg_01_1981.nc \
-          -rpetff /scratch/mo/nest/ulysses/data/meteo/era5land/1982/01/pet_01_1981.nc \
+          -pff    /scratch/mo/nest/ulysses/data/meteo/era5land/1982/01/precipitation_daily_01_1982.nc \
+          -tff    /scratch/mo/nest/ulysses/data/meteo/era5land/1982/01/tavg_01_1982.nc \
+          -rpetff /scratch/mo/nest/ulysses/data/meteo/era5land/1982/01/pet_01_1982.nc \
           end_of_arguments
