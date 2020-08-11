bc7dec64f73ed15864baab17ec0cdf7abcc695b1
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-11 09:56:00 +0200

    add merging for ulyssses

--- model/merge_netcdf_6_arcmin.py
+++ model/merge_netcdf_6_arcmin.py
@@ -447,7 +447,7 @@ ncores = min(len(netcdfList), max_number_of_cores)
 # clone areas
 areas = str(sys.argv[10])
 if areas == "Global":
-    areas = ['M%02d'%i for i in range(1,54,1)]
+    areas = ['M%02d'%i for i in range(1,55,1)]
 else:
     areas = list(set(areas.split(",")))
 
