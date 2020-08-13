6485f34ebddbd7583a2c453285412ab85f1e13b6
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-13 10:40:13 +0200

    fixing clones

--- config/ulysses/version_2020-08-12/main_for_submitting_jobs.sh
+++ config/ulysses/version_2020-08-12/main_for_submitting_jobs.sh
@@ -5,10 +5,10 @@ set -x
 INI_FILE="/home/ms/copext/cyes/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/config/ulysses/version_2020-08-12/setup_6arcmin_test_version_2020-08-12.ini"
 MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_output_version_2020-08-12/"
 
-qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR={MAIN_OUTPUT_DIR} pbs_parallel_set_1.sh
-qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR={MAIN_OUTPUT_DIR} pbs_parallel_set_2.sh
-qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR={MAIN_OUTPUT_DIR} pbs_parallel_set_3.sh
-qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR={MAIN_OUTPUT_DIR} pbs_parallel_set_4.sh
-qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR={MAIN_OUTPUT_DIR} pbs_parallel_set_5.sh
+qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR=${MAIN_OUTPUT_DIR} pbs_parallel_set_1.sh
+qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR=${MAIN_OUTPUT_DIR} pbs_parallel_set_2.sh
+qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR=${MAIN_OUTPUT_DIR} pbs_parallel_set_3.sh
+qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR=${MAIN_OUTPUT_DIR} pbs_parallel_set_4.sh
+qsub -v INI_FILE=${INI_FILE},MAIN_OUTPUT_DIR=${MAIN_OUTPUT_DIR} pbs_parallel_set_5.sh
 
 set +x
