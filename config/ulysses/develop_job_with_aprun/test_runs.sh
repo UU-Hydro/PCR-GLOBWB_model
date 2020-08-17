03f1eb249e9846b0fe8fad0852758593d2ca9ea9
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-17 16:29:32 +0200

    try to understand aprun

--- config/ulysses/develop_job_with_aprun/test_runs.sh
+++ config/ulysses/develop_job_with_aprun/test_runs.sh
@@ -9,6 +9,7 @@ module load gdal/3.0.4
 
 # go to the folder that contain PCR-GLOBWB scripts
 cd /home/ms/copext/cyes/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model/
+pwd
 
 INI_FILE="/home/ms/copext/cyes/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/config/ulysses/version_2020-08-14/continue_from_1996/setup_6arcmin_test_version_2020-08-14_continue_from_1996.ini"
 #~ MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_output_version_2020-08-14_test_with_aprun/continue_from_1996/"
@@ -16,7 +17,9 @@ INI_FILE="/home/ms/copext/cyes/github/edwinkost/PCR-GLOBWB_model_edwin-private-d
 MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/test_aprun/"
 
 # run the model for every clone
+set -x
 CLONE_CODE='d $ALPS_APP_PE'
-python3 deterministic_runner_parallel_for_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} -mod ${MAIN_OUTPUT_DIR} &
+python3 deterministic_runner_parallel_for_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} -mod ${MAIN_OUTPUT_DIR}
+set +x
 
 #~ set +x
