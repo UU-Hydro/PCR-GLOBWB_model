1232356e0816f1198b4debb472d2cae84825c100
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-17 14:06:30 +0200

    example for merging netcdf

--- config/ulysses/merging_example/merge_netcdf_6_arcmin_example_bash.sh
+++ config/ulysses/merging_example/merge_netcdf_6_arcmin_example_bash.sh
@@ -28,6 +28,6 @@ cd /home/ms/copext/cyes/github/edwinkost/PCR-GLOBWB_model_edwin-private-developm
 
 MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_output_version_2020-08-10_example/first_test_54_clones/"
 
-python merge_netcdf_6_arcmin_ulysses.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/global/netcdf outDailyTotNC 1981-01-01 1981-01-31 ulyssesP,ulyssesET,ulyssesSWE,ulyssesQsm,ulyssesSM,ulyssesQrRunoff,ulyssesDischarge NETCDF4 False 8 Global
+python3 merge_netcdf_6_arcmin_ulysses.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/global/netcdf outDailyTotNC 1981-01-01 1981-01-31 ulyssesP,ulyssesET,ulyssesSWE,ulyssesQsm,ulyssesSM,ulyssesQrRunoff,ulyssesDischarge NETCDF4 False 8 Global
 
 set +x
