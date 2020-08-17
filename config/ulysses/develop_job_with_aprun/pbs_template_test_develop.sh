eb8fc8656b146b5bd3fffd07a82e373953f9d955
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-18 00:41:40 +0200

    developing with aprun

--- config/ulysses/develop_job_with_aprun/pbs_template_test_develop.sh
+++ config/ulysses/develop_job_with_aprun/pbs_template_test_develop.sh
@@ -15,9 +15,6 @@
 
 set -x
 
-# save the current working directory
-CWD=$(pwd)
-
 # set the folder that contain PCR-GLOBWB model scripts
 PCRGLOBWB_MODEL_SCRIPT_FOLDER=
 
@@ -46,8 +43,12 @@ REF_POT_ET_FORCING_FILE="/scratch/mo/nest/ulysses/data/meteo/era5land/1982/01/pe
 
 
 # go to the folder that contain the bash script that will be submitted using aprun
-#~ cd /home/ms/copext/cyes/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/config/ulysses/develop_job_with_aprun/
-cd ${CWD}
+
+# set the working directory
+#~ WORKING_DIRECTORY=/home/ms/copext/cyes/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/config/ulysses/develop_job_with_aprun/
+#~ cd ${WORKING_DIRECTORY} 
+
+cd ${PBS_O_WORKDIR}
 
 # make the run for every clone using aprun
 aprun -N $EC_tasks_per_node -n $EC_total_tasks -j $EC_hyperthreads bash pcrglobwb_runs.sh ${INI_FILE} ${MAIN_OUTPUT_DIR} ${STARTING_DATE} ${END_DATE} ${MAIN_INITIAL_STATE_FOLDER} ${DATE_FOR_INITIAL_STATES} ${PRECIPITATION_FORCING_FILE} ${TEMPERATURE_FORCING_FILE} ${REF_POT_ET_FORCING_FILE} ${PCRGLOBWB_MODEL_SCRIPT_FOLDER}
@@ -57,7 +58,7 @@ aprun -N $EC_tasks_per_node -n $EC_total_tasks -j $EC_hyperthreads bash pcrglobw
 # - go to the folder that contain the scripts
 cd ${PCRGLOBWB_MODEL_SCRIPT_FOLDER}
 # - merging state files
-python merge_pcraster_maps_6_arcmin_ulysses.py 1995-12-31 ${MAIN_OUTPUT_DIR} states 2 Global 54 False
+python merge_pcraster_maps_6_arcmin_ulysses.py ${END_DATE} ${MAIN_OUTPUT_DIR} states 2 Global 54 False
 # - merging netcdf files
 python merge_netcdf_6_arcmin_ulysses.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/global/netcdf outDailyTotNC ${STARTING_DATE} ${END_DATE} ulyssesP,ulyssesET,ulyssesSWE,ulyssesQsm,ulyssesSM,ulyssesQrRunoff,ulyssesDischarge NETCDF4 False 2 Global
 
