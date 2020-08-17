b8dce032ba8d31f007d90bb34df41026387f8f33
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-17 23:40:36 +0200

    developing with aprun

---
+++ config/ulysses/develop_job_with_aprun/pbs_test_nf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#PBS -N test-nf
+#PBS -q nf
+#PBS -l EC_billing_account=c3s432l3
+
+#PBS -l walltime=48:00:00
+#~ #PBS -l walltime=8:00
+
+#PBS -M hsutanudjajacchms99@yahoo.com
+
+# 
+
+set -x
+
+pwd
+
+set +x
