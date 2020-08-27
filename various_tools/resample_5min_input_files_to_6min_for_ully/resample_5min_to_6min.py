2faeda01c84f3a0f686303c2502a22139a80c87f
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-27 19:40:27 +0200

    resample standard input to 6 min using remapcon

--- various_tools/resample_5min_input_files_to_6min_for_ully/resample_5min_to_6min.py
+++ various_tools/resample_5min_input_files_to_6min_for_ully/resample_5min_to_6min.py
@@ -15,6 +15,7 @@ def main():
 
     #~ file_list = "list_of_selected_global_nc_5min_files_version_2019_11_beta.txt"
     file_list    = "list_of_global_nc_opendap_files_version_2019_11_beta.txt"
+    file_list    = "list_of_selected_global_nc_opendap_files_version_2019_11_beta.txt"
 
     opendap_main_folder =  "https://opendap.4tu.nl/thredds/dodsC/data2/pcrglobwb/version_2019_11_beta/pcrglobwb2_input/"
 
@@ -25,39 +26,41 @@ def main():
     
     for opendap_filename in filenames:
 
-        # opendap file name
-        opendap_filename = opendap_filename.replace("\n", "")
+        if opendap_filename.startswith("#") == False:
         
-        # local file name
-        local_file_name  = opendap_filename.replace(opendap_main_folder, source_local_folder)
-        
-        # target file name
-        target_file_name = opendap_filename.replace(opendap_main_folder, target_local_folder)
-                
-        # target file name
-        target_file_name = target_file_name.replace("global_05min", "global_06min")
-
-        # preparing directory 
-        target_directory = os.path.dirname(target_file_name)
-        if os.path.exists(target_directory) == False:
-            os.makedirs(target_directory)
-        
-        print("\n\n")
-
-        # perform cdo remapcon
-
-        cmd_line = "cdo -L -remapcon,griddes_land_mask_only.nc.txt " + " " + local_file_name + " " + target_file_name
-        print(cmd_line)
-        os.system(cmd_line)
-        
-        # check if the file is produced
-        msg = "\n"        
-        if os.path.exists(target_file_name):
-            msg += "The file " + target_file_name + " is succesfuly created."
-        else: 
-            msg += "ERROR: The file " + target_file_name + " can NOT BE created."
-        msg += "\n"        
-        print(msg)    
+            # opendap file name
+            opendap_filename = opendap_filename.replace("\n", "")
+            
+            # local file name
+            local_file_name  = opendap_filename.replace(opendap_main_folder, source_local_folder)
+            
+            # target file name
+            target_file_name = opendap_filename.replace(opendap_main_folder, target_local_folder)
+                    
+            # target file name
+            target_file_name = target_file_name.replace("global_05min", "global_06min")
+            
+            # preparing directory 
+            target_directory = os.path.dirname(target_file_name)
+            if os.path.exists(target_directory) == False:
+                os.makedirs(target_directory)
+            
+            print("\n\n")
+            
+            # perform cdo remapcon
+            
+            cmd_line = "cdo -L -remapcon,griddes_land_mask_only.nc.txt " + " " + local_file_name + " " + target_file_name
+            print(cmd_line)
+            os.system(cmd_line)
+            
+            # check if the file is produced
+            msg = "\n"        
+            if os.path.exists(target_file_name):
+                msg += "The file " + target_file_name + " is succesfuly created."
+            else: 
+                msg += "ERROR: The file " + target_file_name + " can NOT BE created."
+            msg += "\n"        
+            print(msg)    
 
     txt_file_list.close()
     
