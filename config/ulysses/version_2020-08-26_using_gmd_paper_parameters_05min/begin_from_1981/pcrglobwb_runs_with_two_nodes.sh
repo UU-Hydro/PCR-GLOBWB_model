ce13d4bdaf67697d1abb0f153a076cdabd157db5
Edwin Sutanudjaja <edwin@cartesius.surfsara.nl>
2020-08-27 11:28:07 +0200

    version_2020-08-26_using_gmd_paper_parameters_05min with era5land forcing

--- config/ulysses/version_2020-08-26_using_gmd_paper_parameters_05min/begin_from_1981/pcrglobwb_runs_with_two_nodes.sh
+++ config/ulysses/version_2020-08-26_using_gmd_paper_parameters_05min/begin_from_1981/pcrglobwb_runs_with_two_nodes.sh
@@ -40,12 +40,12 @@ cd ${PCRGLOBWB_MODEL_SCRIPT_FOLDER}
 # - first node
 if [ $ALPS_APP_PE -gt 0 ]
 then
-if [ $ALPS_APP_PE -lt 19 ]
+if [ $ALPS_APP_PE -lt 18 ]
 then
 #~ set -x
 CLONENUMBER=$ALPS_APP_PE
 CLONE_CODE=`printf %d $CLONENUMBER`
-python3 deterministic_runner_parallel_for_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} -mod ${MAIN_OUTPUT_DIR} -sd ${STARTING_DATE} -ed ${END_DATE} -misd ${MAIN_INITIAL_STATE_FOLDER} -dfis ${DATE_FOR_INITIAL_STATES} -pff ${PRECIPITATION_FORCING_FILE} -tff ${TEMPERATURE_FORCING_FILE} -rpetff ${REF_POT_ET_FORCING_FILE}
+python3 deterministic_runner_parallel_for_ulysses.py ${INI_FILE} parallel ${CLONE_CODE} -mod ${MAIN_OUTPUT_DIR} -sd ${STARTING_DATE} -ed ${END_DATE} -misd ${MAIN_INITIAL_STATE_FOLDER} -dfis ${DATE_FOR_INITIAL_STATES} -pff ${PRECIPITATION_FORCING_FILE} -tff ${TEMPERATURE_FORCING_FILE} -rpetff ${REF_POT_ET_FORCING_FILE}
 echo $CLONENUMBER
 echo ${CLONE_CODE}
 #~ set +x
@@ -56,14 +56,14 @@ fi
 if [ $ALPS_APP_PE -gt 72 ]
 then
 CLONENUMBER=$((ALPS_APP_PE-72))
-if [ $CLONENUMBER -gt 18 ]
+if [ $CLONENUMBER -gt 17 ]
 then
 #~ if [ $CLONENUMBER -lt 55 ]
 if [ $CLONENUMBER -lt 54 ] # for 5 arcmin gmd run (containing only 53 clones)
 then
 #~ set -x
 CLONE_CODE=`printf %d ${CLONENUMBER}`
-python3 deterministic_runner_parallel_for_ulysses.py ${INI_FILE} debug_parallel ${CLONE_CODE} -mod ${MAIN_OUTPUT_DIR} -sd ${STARTING_DATE} -ed ${END_DATE} -misd ${MAIN_INITIAL_STATE_FOLDER} -dfis ${DATE_FOR_INITIAL_STATES} -pff ${PRECIPITATION_FORCING_FILE} -tff ${TEMPERATURE_FORCING_FILE} -rpetff ${REF_POT_ET_FORCING_FILE}
+python3 deterministic_runner_parallel_for_ulysses.py ${INI_FILE} parallel ${CLONE_CODE} -mod ${MAIN_OUTPUT_DIR} -sd ${STARTING_DATE} -ed ${END_DATE} -misd ${MAIN_INITIAL_STATE_FOLDER} -dfis ${DATE_FOR_INITIAL_STATES} -pff ${PRECIPITATION_FORCING_FILE} -tff ${TEMPERATURE_FORCING_FILE} -rpetff ${REF_POT_ET_FORCING_FILE}
 echo $CLONENUMBER
 echo ${CLONE_CODE}
 #~ set +x
