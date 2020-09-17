798dfe2d353a37aed9e184d8c49c4d708494e744
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-09-17 22:46:48 +0200

    senegal_v_2020-09-XX_chrips/main_remapcon_arise_chirps_precipitation.sh

--- various_tools/remapcon_arise/senegal/senegal_v_2020-09-XX_chrips/main_remapcon_arise_chirps_precipitation.sh
+++ various_tools/remapcon_arise/senegal/senegal_v_2020-09-XX_chrips/main_remapcon_arise_chirps_precipitation.sh
@@ -22,16 +22,16 @@ mkdir -p ${MAIN_OUT_FOLDER}
 # - 30 arcmin resolution
 RESOLUTION=0.5
 OUT_FOLDER=${MAIN_OUT_FOLDER}/half_arcdeg/
-bash remapcon_arise.sh ${INP_FOLDER}/chirps_rainfall/SNL_chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} senegal_chirps-v2.0_1981-2020_p05_rempacon-30-arcmin monsum mm.month-1
+bash remapcon_arise.sh ${INP_FOLDER}/SNL_chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} senegal_chirps-v2.0_1981-2020_p05_rempacon-30-arcmin monsum mm.month-1
 
 # - 5 arcmin resolution
 RESOLUTION=0.083333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
 OUT_FOLDER=${MAIN_OUT_FOLDER}/5_arcmin/
-bash remapcon_arise.sh ${INP_FOLDER}/chirps_rainfall/SNL_chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} senegal_chirps-v2.0_1981-2020_p05_rempacon-5-arcmin monsum mm.month-1
+bash remapcon_arise.sh ${INP_FOLDER}/SNL_chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} senegal_chirps-v2.0_1981-2020_p05_rempacon-5-arcmin monsum mm.month-1
 
 # - 150 arcsec resolution
 RESOLUTION=0.041666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666667
 OUT_FOLDER=${MAIN_OUT_FOLDER}/150_arcsec/
-bash remapcon_arise.sh ${INP_FOLDER}/chirps_rainfall/SNL_chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} senegal_chirps-v2.0_1981-2020_p05_rempacon-150-arcsec monsum mm.month-1
+bash remapcon_arise.sh ${INP_FOLDER}/SNL_chirps-v2.0.1981-2020.days_p05.nc ${RESOLUTION} ${OUT_FOLDER} senegal_chirps-v2.0_1981-2020_p05_rempacon-150-arcsec monsum mm.month-1
 
 set +x
