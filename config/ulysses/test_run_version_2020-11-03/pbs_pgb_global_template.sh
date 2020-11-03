4bd12b1116b72d49da51c9065f781efba988aebb
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-11-03 01:24:21 +0100

    test with new forcing

--- config/ulysses/test_run_version_2020-11-03/pbs_pgb_global_template.sh
+++ config/ulysses/test_run_version_2020-11-03/pbs_pgb_global_template.sh
@@ -30,7 +30,7 @@ PCRGLOBWB_MODEL_SCRIPT_FOLDER="/perm/mo/nest/ulysses/src/edwin/ulysses_pgb_sourc
 INI_FILE=${PBS_O_WORKDIR}/"setup_6arcmin_version_develop.ini"
 
 # set the output folder
-MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/test_run_version_2020-10-29/default/"
+MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/reference_run_version_2020-10-29/default/"
 
 # set the starting and end simulation dates
 STARTING_DATE=1981-01-01
