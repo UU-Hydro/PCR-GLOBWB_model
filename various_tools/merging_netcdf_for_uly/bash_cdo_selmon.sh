84a4e69d62a9c30978fb42acac51e3d893766e61
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-12-08 14:51:58 +0100

    split runoff to monthly files

---
+++ various_tools/merging_netcdf_for_uly/bash_cdo_selmon.sh
@@ -0,0 +1,86 @@
+
+set -x
+
+bash cdo_selmon.sh 1981
+bash cdo_selmon.sh 1982
+bash cdo_selmon.sh 1983
+bash cdo_selmon.sh 1984
+bash cdo_selmon.sh 1985
+bash cdo_selmon.sh 1986
+bash cdo_selmon.sh 1987
+bash cdo_selmon.sh 1988
+bash cdo_selmon.sh 1989
+bash cdo_selmon.sh 1990
+bash cdo_selmon.sh 1991
+bash cdo_selmon.sh 1992
+bash cdo_selmon.sh 1993
+bash cdo_selmon.sh 1994
+bash cdo_selmon.sh 1995
+bash cdo_selmon.sh 1996
+bash cdo_selmon.sh 1997
+bash cdo_selmon.sh 1998
+bash cdo_selmon.sh 1999
+bash cdo_selmon.sh 2000
+bash cdo_selmon.sh 2001
+bash cdo_selmon.sh 2002
+bash cdo_selmon.sh 2003
+bash cdo_selmon.sh 2004
+bash cdo_selmon.sh 2005
+bash cdo_selmon.sh 2006
+bash cdo_selmon.sh 2007
+bash cdo_selmon.sh 2008
+bash cdo_selmon.sh 2009
+bash cdo_selmon.sh 2010
+bash cdo_selmon.sh 2011
+bash cdo_selmon.sh 2012
+bash cdo_selmon.sh 2013
+bash cdo_selmon.sh 2014
+bash cdo_selmon.sh 2015
+bash cdo_selmon.sh 2016
+bash cdo_selmon.sh 2017
+bash cdo_selmon.sh 2018
+bash cdo_selmon.sh 2019
+                   
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1981
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1982
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1983
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1984
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1985
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1986
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1987
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1988
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 19:33 1989
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1990
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1991
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1992
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1993
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1994
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1995
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1996
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1997
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1998
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 21:15 1999
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:39 2000
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2001
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2002
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2003
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2004
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2005
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2006
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2007
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2008
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  7 22:40 2009
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2010
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2011
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2012
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2013
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2014
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2015
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2016
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2017
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2018
+#~ dr-xr-x---  2 cynw copext 4.0K Dec  8 00:08 2019
+#~ -r--r-----  1 cynw copext  19K Dec  7 19:33 merge_netcdf_6_arcmin_ulysses.py
+#~ (pcrglobwb_python36_ulysses) cynw@cca-login4:/scratch/ms/copext/cynw/pcrglobwb_ulysses_reference_runs_version_2020-12-01/old-jgw_uly-et0_uly-lcv_merged_1981-2019/runoff>
+
+set +x
---
+++ various_tools/merging_netcdf_for_uly/cdo_selmon.sh
@@ -0,0 +1,27 @@
+set -x
+
+YEAR=$1
+
+OUT_FOLDER="/scratch/ms/copext/cynw/pcrglobwb_ulysses_reference_runs_version_2020-12-01/old-jgw_uly-et0_uly-lcv_merged_1981-2019/daily_runoff_in_monthly_files/"
+cd ${OUT_FOLDER}
+
+INP_FOLDER="/scratch/ms/copext/cynw/pcrglobwb_ulysses_reference_runs_version_2020-12-01/old-jgw_uly-et0_uly-lcv_merged_1981-2019/runoff/"
+
+mkdir ${YEAR}
+
+cdo -L -f nc4 -selmon,1  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01.nc &
+cdo -L -f nc4 -selmon,2  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-02.nc &
+cdo -L -f nc4 -selmon,3  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-03.nc &
+cdo -L -f nc4 -selmon,4  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-04.nc &
+cdo -L -f nc4 -selmon,5  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-05.nc &
+cdo -L -f nc4 -selmon,6  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-06.nc &
+cdo -L -f nc4 -selmon,7  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-07.nc &
+cdo -L -f nc4 -selmon,8  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-08.nc &
+cdo -L -f nc4 -selmon,9  ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-09.nc &
+cdo -L -f nc4 -selmon,10 ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-10.nc &
+cdo -L -f nc4 -selmon,11 ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-11.nc &
+cdo -L -f nc4 -selmon,12 ${INP_FOLDER}/${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-01-01_to_${YEAR}-12-31.nc ${YEAR}/ulyssesQrRunoff_dailyTot_output_${YEAR}-12.nc &
+
+wait
+
+set +x
