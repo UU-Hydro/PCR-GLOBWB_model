514ad52c6a192698add0a5353f907e212d71ef47
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-01-09 00:23:22 +0100

    start again the runs

--- config/ulysses/reference_runs_version_2021-01-XX/all_runs.sh
+++ config/ulysses/reference_runs_version_2021-01-XX/all_runs.sh
@@ -6,6 +6,6 @@ bash complete_job_scripts_for_a_run.sh "1.00" "1.00" "gpu034"
 bash complete_job_scripts_for_a_run.sh "1.05" "1.05" "gpu035"
 bash complete_job_scripts_for_a_run.sh "1.10" "1.10" "gpu036"
 bash complete_job_scripts_for_a_run.sh "1.25" "1.25" "gpu037"
-bash complete_job_scripts_for_a_run.sh "1.50" "1.50" "gpu048"
+bash complete_job_scripts_for_a_run.sh "1.50" "1.50" "gpu038"
 
 set +x
