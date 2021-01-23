9e9b686be89cbe1595c89eb0248abd795c041c46
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-01-23 22:21:12 +0100

    test new et0

--- model/meteo.py
+++ model/meteo.py
@@ -1024,4 +1024,4 @@ class Meteo(object):
                 vars(self)[meteo_var_name] = vars(self)['consta_for_' + meteo_var_name] + \
                                              vars(self)['factor_for_' + meteo_var_name] * pcr.ifthen(self.landmask, vars(self)[meteo_var_name])                                                   
 
-        pcr.aguila(self.relative_humidity)  
+        # ~ pcr.aguila(self.relative_humidity)
