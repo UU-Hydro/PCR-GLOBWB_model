baaace650f60101235ea8e3ea61aa6f84729c892
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-02-18 23:05:30 +0100

    test a general node on cx1

--- config/30sec_african_countries/tanzania/version_20191211/etc/pbs_test.sh
+++ config/30sec_african_countries/tanzania/version_20191211/etc/pbs_test.sh
@@ -8,6 +8,8 @@
 cd /rds/general/user/esutanud/home/
 . load_all_default.sh
 
+export PCRASTER_NR_WORKER_THREADS=8
+
 cd /rds/general/user/esutanud/home/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model/
 python deterministic_runner.py ../config/30sec_african_countries/tanzania/version_20191211/setup_30sec_tanzania-basin_test_version_2019_12_11_rerun.ini debug
 
