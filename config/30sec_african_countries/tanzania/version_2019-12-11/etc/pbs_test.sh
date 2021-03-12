3e053dae8c823e0fd00df30fcaba9063d29b791c
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-12 09:12:21 +0100

    start testing the new tanzania run

---
+++ config/30sec_african_countries/tanzania/version_2019-12-11/etc/pbs_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=32:mem=62gb
+
+#~ #PBS -q express -P exp-0032
+
+cd /rds/general/user/esutanud/home/
+. load_all_default.sh
+
+export PCRASTER_NR_WORKER_THREADS=8
+
+cd /rds/general/user/esutanud/home/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model/
+python deterministic_runner.py ../config/30sec_african_countries/tanzania/version_20191211/setup_30sec_tanzania-basin_test_version_2019_12_11_rerun.ini debug
+
---
+++ config/30sec_african_countries/tanzania/version_2019-12-11/pbs_test_singlenode.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=48:mem=124gb
+#PBS -l walltime=24:00:00
+
+#~ #PBS -q express -P exp-0032
+
+# load all software needed
+cd /rds/general/user/esutanud/home/
+. load_all_default.sh
+
+# set number of threads for pcraster
+export PCRASTER_NR_WORKER_THREADS=8
+
+# execute the model
+cd /rds/general/user/esutanud/home/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model/
+python deterministic_runner.py ../config/30sec_african_countries/tanzania/version_20191211/setup_30sec_tanzania-basin_test_version_2019_12_11_rerun_singlenode.ini debug
