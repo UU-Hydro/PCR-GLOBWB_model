3313debeb3bdab0fafe406a42c5f622012eb7469
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-04-20 15:32:45 +0200

    in progress for europe 30sec

--- config/30sec_europe/version_2021-04-XX_for_egu_30sec/all_runs_europe_30sec.sh
+++ config/30sec_europe/version_2021-04-XX_for_egu_30sec/all_runs_europe_30sec.sh
@@ -8,26 +8,26 @@ set -x
 
 
 
-sbatch -J 01030000 --exportPCRTHREADS="24",CLONE1="3",CLONE2="0",CLONE3="0"   job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 02010000 --exportPCRTHREADS="24",CLONE1="1",CLONE2="0",CLONE3="0"   job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 03130000 --exportPCRTHREADS="24",CLONE1="13",CLONE2="0",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 04100000 --exportPCRTHREADS="24",CLONE1="10",CLONE2="0",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 05302500 --exportPCRTHREADS="12",CLONE1="30",CLONE2="25",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 06193600 --exportPCRTHREADS="12",CLONE1="19",CLONE2="36",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 07062200 --exportPCRTHREADS="12",CLONE1="6",CLONE2="22",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 08390200 --exportPCRTHREADS="12",CLONE1="39",CLONE2="2",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 09441400 --exportPCRTHREADS="12",CLONE1="44",CLONE2="14",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 10231600 --exportPCRTHREADS="12",CLONE1="23",CLONE2="16",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 11340500 --exportPCRTHREADS="12",CLONE1="34",CLONE2="5",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 12184038 --exportPCRTHREADS="8",CLONE1="18",CLONE2="40",CLONE3="38" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 13330929 --exportPCRTHREADS="8",CLONE1="33",CLONE2="9",CLONE3="29"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 14122043 --exportPCRTHREADS="8",CLONE1="12",CLONE2="20",CLONE3="43" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 15262731 --exportPCRTHREADS="8",CLONE1="26",CLONE2="27",CLONE3="31" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 16371724 --exportPCRTHREADS="8",CLONE1="37",CLONE2="17",CLONE3="24" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 17074542 --exportPCRTHREADS="8",CLONE1="7",CLONE2="45",CLONE3="42"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 18080441 --exportPCRTHREADS="8",CLONE1="8",CLONE2="4",CLONE3="41"   job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 19152128 --exportPCRTHREADS="8",CLONE1="15",CLONE2="21",CLONE3="28" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
-sbatch -J 20113235 --exportPCRTHREADS="8",CLONE1="11",CLONE2="32",CLONE3="35" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 01030000 --export PCRTHREADS="24",CLONE1="3",CLONE2="0",CLONE3="0"   job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 02010000 --export PCRTHREADS="24",CLONE1="1",CLONE2="0",CLONE3="0"   job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 03130000 --export PCRTHREADS="24",CLONE1="13",CLONE2="0",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 04100000 --export PCRTHREADS="24",CLONE1="10",CLONE2="0",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 05302500 --export PCRTHREADS="12",CLONE1="30",CLONE2="25",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 06193600 --export PCRTHREADS="12",CLONE1="19",CLONE2="36",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 07062200 --export PCRTHREADS="12",CLONE1="6",CLONE2="22",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 08390200 --export PCRTHREADS="12",CLONE1="39",CLONE2="2",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 09441400 --export PCRTHREADS="12",CLONE1="44",CLONE2="14",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 10231600 --export PCRTHREADS="12",CLONE1="23",CLONE2="16",CLONE3="0" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 11340500 --export PCRTHREADS="12",CLONE1="34",CLONE2="5",CLONE3="0"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 12184038 --export PCRTHREADS="8",CLONE1="18",CLONE2="40",CLONE3="38" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 13330929 --export PCRTHREADS="8",CLONE1="33",CLONE2="9",CLONE3="29"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 14122043 --export PCRTHREADS="8",CLONE1="12",CLONE2="20",CLONE3="43" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 15262731 --export PCRTHREADS="8",CLONE1="26",CLONE2="27",CLONE3="31" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 16371724 --export PCRTHREADS="8",CLONE1="37",CLONE2="17",CLONE3="24" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 17074542 --export PCRTHREADS="8",CLONE1="7",CLONE2="45",CLONE3="42"  job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 18080441 --export PCRTHREADS="8",CLONE1="8",CLONE2="4",CLONE3="41"   job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 19152128 --export PCRTHREADS="8",CLONE1="15",CLONE2="21",CLONE3="28" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+sbatch -J 20113235 --export PCRTHREADS="8",CLONE1="11",CLONE2="32",CLONE3="35" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
 
 #~ 24	01030000 ! 3		   !
 #~ 24	02010000 ! 1		   !
