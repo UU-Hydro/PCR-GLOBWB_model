1ab9fc5da9a9311203b9280e654e787bdd62d495
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-04-20 13:54:09 +0200

    in progress for europe 30sec

---
+++ config/30sec_europe/version_2021-04-XX_for_egu_30sec/all_runs_europe_30sec_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+sbatch -J eu113235 - -export PCRTHREADS="8",CLONE1="11",CLONE2="32",CLONE3="35" job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+
+
+set +x
--- config/30sec_europe/version_2021-04-XX_for_egu_30sec/job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+++ config/30sec_europe/version_2021-04-XX_for_egu_30sec/job_script_sbatch_pcrglobwb_europe_30sec_template.sh
@@ -12,9 +12,13 @@
 
 #SBATCH --export PCRTHREADS="8",CLONE1="0",CLONE2="0",CLONE3="0"
 
-set -x
+#~ edwinari@tcn757.bullx:/home/edwinari/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/config/30sec_europe/version_2021-04-XX_for_egu_30sec$ ls -lah *
+#~ -rw-r--r-- 1 edwinari edwinari 728 Apr 20 13:47 job_script_sbatch_pcrglobwb_europe_30sec_template.sh
+#~ -rw-r--r-- 1 edwinari edwinari 37K Apr 20 13:47 setup_30sec_europe_version_2021-04-XX.ini
 
-CONFIG_INI_FILE=""
+CONFIG_INI_FILE="/home/edwinari/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/config/30sec_europe/version_2021-04-XX_for_egu_30sec/setup_30sec_europe_version_2021-04-XX.ini"
+
+set -x
 
 # load all required modules
 . /home/edwinari/load_my_miniconda_and_my_default_env.sh
@@ -24,10 +28,10 @@ export PCRASTER_NR_WORKER_THREADS=${PCRTHREADS}
 # go to the model folder
 cd /home/edwinari/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model
 
-
-
-python deterministic_runner_parallel_for_ulysses.py ../config/30sec_europe/setup_30sec_europe_develop.ini debug_parallel 3
-
+# run the model(s)
+python deterministic_runner_parallel_for_ulysses.py ${CONFIG_INI_FILE} parallel ${CLONE1} &
+python deterministic_runner_parallel_for_ulysses.py ${CONFIG_INI_FILE} parallel ${CLONE2} &
+python deterministic_runner_parallel_for_ulysses.py ${CONFIG_INI_FILE} parallel ${CLONE3} &
 
 wait
 
