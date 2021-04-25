6f7b6a6bfcafc222a612fd19d2a9121daf542e05
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-04-26 00:20:15 +0200

    merging europe

--- model/merge_netcdf_europe.py
+++ model/merge_netcdf_europe.py
@@ -376,12 +376,12 @@ def mergeNetCDF(inputTuple):
             posCnt= None
             try:
                 
-                #~ # find the correct index (old method) - this is very slow
-                #~ posCnt= variables[index]['time'][:].tolist().index(time)
+                # find the correct index (old method) - this is very slow
+                posCnt= variables[index]['time'][:].tolist().index(time)
                 
-                # find the correct index (new method)
-                date_value = nc.num2date(time, rootgrp.variables['time'].units, rootgrp.variables['time'].calendar)
-                posCnt = nc.date2index(date_value, rootgrp.variables['time'])
+                # ~ # find the correct index (new method)
+                # ~ date_value = nc.num2date(time, rootgrp.variables['time'].units, rootgrp.variables['time'].calendar)
+                # ~ posCnt = nc.date2index(date_value, rootgrp.variables['time'])
                 
                 sampleArray= rootgrp.variables[variableName][posCnt,:,:]
                 sampleArray[sampleArray == variables[index][variableName]._FillValue]= MV
