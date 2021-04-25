ac90374e8a539f6a9db319989d1ce77a342cc9d3
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-04-26 01:04:27 +0200

    merging europe

--- model/merge_netcdf_30arcsec_europe_example_bash.sh
+++ model/merge_netcdf_30arcsec_europe_example_bash.sh
@@ -4,13 +4,13 @@ set -x
 
 MAIN_OUTPUT_DIR="/lustre1/0/einf1079/edwin/pcrglobwb_output_europe/europe_30sec/version_2021-04-20/"
 
-#~ python merge_netcdf_europe.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/europe_30sec/netcdf outMonthTotNC 1981-01-01 1984-12-31 runoff,totalRunoff,precipitation,gwRecharge,surfaceWaterInf,referencePotET,totalEvaporation,totalPotentialEvaporation NETCDF4 False 12 europe_30sec europe_30sec &
+python merge_netcdf_europe.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/europe_30sec/netcdf outMonthTotNC 1981-01-01 1984-12-31 runoff,totalRunoff,precipitation,gwRecharge,surfaceWaterInf,referencePotET,totalEvaporation,totalPotentialEvaporation NETCDF4 False 12 europe_30sec europe_30sec &
 
-#~ python merge_netcdf_europe.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/europe_30sec/netcdf outMonthAvgNC 1981-01-01 1984-12-31 discharge,temperature,snowCoverSWE,storUppTotal,storLowTotal,totalWaterStorageThickness,satDegUpp,satDegLow NETCDF4 False 12 europe_30sec europe_30sec &
+python merge_netcdf_europe.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/europe_30sec/netcdf outMonthAvgNC 1981-01-01 1984-12-31 discharge,temperature,snowCoverSWE,storUppTotal,storLowTotal,totalWaterStorageThickness,satDegUpp,satDegLow NETCDF4 False 12 europe_30sec europe_30sec &
 
 wait
 
-python merge_netcdf_europe.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/europe_30sec/netcdf outMonthAvgNC 1981-01-31 1981-03-31 discharge NETCDF4 False 12 europe_30sec europe_30sec 
+#~ python merge_netcdf_europe.py ${MAIN_OUTPUT_DIR} ${MAIN_OUTPUT_DIR}/europe_30sec/netcdf outMonthAvgNC 1981-01-31 1981-03-31 discharge NETCDF4 False 12 europe_30sec europe_30sec 
 
 
 
