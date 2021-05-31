8709f5208228072d5bbc68453b0b005c6c86093f
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-05-31 12:05:15 +0200

    kinematicwave

---
+++ config/05min_africa/version_2021-05-31/pbs_africa_version_2021-05-31_kinematicwave.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#PBS -N af5m_kwv
+
+#PBS -l select=1:ncpus=48:mem=124gb
+#PBS -l walltime=48:00:00
+
+#PBS -q express -P exp-00044
+
+# load all software needed
+cd /rds/general/user/esutanud/home/
+. load_all_default.sh
+
+# set number of threads for pcraster
+export PCRASTER_NR_WORKER_THREADS=48
+
+# set the folder that contain PCR-GLOBWB model scripts
+PCRGLOBWB_MODEL_SCRIPT_FOLDER="/rds/general/user/esutanud/home/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model/"
+
+# set the configuration file (*.ini) that will be used (assumption: the .ini file is located within the same directory as this job, i.e. ${PBS_O_WORKDIR})
+INI_FILE=${PBS_O_WORKDIR}/"setup_05min_africa_version_2021-05-31_kinematicwave.ini"
+
+# execute the model
+cd /rds/general/user/esutanud/home/github/edwinkost/PCR-GLOBWB_model_edwin-private-development/model/
+python deterministic_runner.py ${INI_FILE}
