4f8f07f2190a6f8ee8a6b2a4f67bae1892b0ed0e
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-06-02 00:39:36 +0200

    using the job sequence

--- config/05min_africa/version_2021-06-01_with_parallelization/job_sequence.sh
+++ config/05min_africa/version_2021-06-01_with_parallelization/job_sequence.sh
@@ -16,13 +16,14 @@ GENERAL_MAIN_OUTPUT_DIR="/rds/general/user/esutanud/projects/arise/live/HydroMod
 STARTING_YEAR=1981
 END_YEAR=2019
 NUMBER_OF_SPINUP_YEARS="5"
-MAIN_INITIAL_STATE_FOLDER="/rds/general/user/esutanud/projects/arise/live/HydroModelling/edwin/pcrglobwb_output_africa/version_2021-05-31/africa_05min/africa_kinematicwave/states/"
+MAIN_INITIAL_STATE_FOLDER="/rds/general/user/esutanud/projects/arise/live/HydroModelling/edwin/pcrglobwb_output_africa/version_2021-05-31/africa_05min/africa_accutraveltime/states/"
 DATE_FOR_INITIAL_STATES="1981-12-31"
 
 
 # spinup
-SUB_JOBNAME=${JOB_NAME}_spin-up
-MAIN_OUTPUT_DIR=${GENERAL_MAIN_OUTPUT_DIR}/_spinup/with_1981/
+
+SUB_JOBNAME=${JOB_NAME}_spinup
+MAIN_OUTPUT_DIR=${GENERAL_MAIN_OUTPUT_DIR}/_spinup/with_${STARTING_YEAR}/
 STARTING_DATE=${STARTING_YEAR}-01-01
 END_DATE=${STARTING_YEAR}-12-31
 SPIN_UP=$(qsub -N "${SUB_JOBNAME}" -v INI_FILE="${INI_FILE}",MAIN_OUTPUT_DIR="${MAIN_OUTPUT_DIR}",STARTING_DATE="${STARTING_DATE}",END_DATE="${END_DATE}",MAIN_INITIAL_STATE_FOLDER="${MAIN_INITIAL_STATE_FOLDER}",DATE_FOR_INITIAL_STATES="${DATE_FOR_INITIAL_STATES}",NUMBER_OF_SPINUP_YEARS="${NUMBER_OF_SPINUP_YEARS}" pbs_job_script_for_a_run.sh)
