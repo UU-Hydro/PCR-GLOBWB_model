4fe85b4ba94c95b137d8428215206c712c2b49af
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-06-01 22:34:01 +0200

    test with spinup argument - with kinematicwave

---
+++ config/05min_africa/version_2021-06-01_with_parallelization/job_sequence.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#~ # Submit the first job and save the JobID as JOBONE
+#~ JOBONE=$(qsub program1.pbs)
+#~ # Submit the second job, use JOBONE as depend, save JobID
+#~ JOBTWO=$(qsub -W depend=afterok:$JOBONE program2.pbs)
+#~ # Submit last job using JOBTWO as depend, do not need to save the JobID
+#~ qsub -W depend=afterok:$JOBTWO program3.pbs
+
+set -x
+
+JOB_NAME=
+INI_FILE=
+
+J "${SUB_JOBNAME}"
+
+
+SPIN_UP=$(qsub -N nwj_wmup -v INI_FILE="setup_6arcmin_new-jgw_uly-et0_uly-lcv_on_cca_for_spinup.ini",MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/_spinup/begin_from_1981/",STARTING_DATE="1981-01-01",END_DATE="1981-12-31",MAIN_INITIAL_STATE_FOLDER="NONE",DATE_FOR_INITIAL_STATES="NONE" job_script_pbs_pcrglobwb_template.sh)
+
+JOB_001=$(qsub -N nwj_8190 -W depend=afterany:${SPIN_UP} -v INI_FILE="setup_6arcmin_new-jgw_uly-et0_uly-lcv_on_cca_with_initial_states.ini",MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/begin_from_1981/",STARTING_DATE="1981-01-01",END_DATE="1990-12-31",MAIN_INITIAL_STATE_FOLDER="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/_spinup/begin_from_1981/global/states/",DATE_FOR_INITIAL_STATES="1981-12-31" job_script_pbs_pcrglobwb_template.sh)
+
+JOB_002=$(qsub -N nwj_9100 -W depend=afterany:${JOB_001} -v INI_FILE="setup_6arcmin_new-jgw_uly-et0_uly-lcv_on_cca_with_initial_states.ini",MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/continue_from_1991/",STARTING_DATE="1991-01-01",END_DATE="2000-12-31",MAIN_INITIAL_STATE_FOLDER="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/begin_from_1981/global/states/",DATE_FOR_INITIAL_STATES="1990-12-31" job_script_pbs_pcrglobwb_template.sh)
+
+JOB_003=$(qsub -N nwj_0110 -W depend=afterany:${JOB_002} -v INI_FILE="setup_6arcmin_new-jgw_uly-et0_uly-lcv_on_cca_with_initial_states.ini",MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/continue_from_2001/",STARTING_DATE="2001-01-01",END_DATE="2010-12-31",MAIN_INITIAL_STATE_FOLDER="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/continue_from_1991/global/states/",DATE_FOR_INITIAL_STATES="2000-12-31" job_script_pbs_pcrglobwb_template.sh)
+
+JOB_004=$(qsub -N nwj_1119 -W depend=afterany:${JOB_003} -v INI_FILE="setup_6arcmin_new-jgw_uly-et0_uly-lcv_on_cca_with_initial_states.ini",MAIN_OUTPUT_DIR="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/continue_from_2011/",STARTING_DATE="2011-01-01",END_DATE="2019-12-31",MAIN_INITIAL_STATE_FOLDER="/scratch/ms/copext/cyes/pcrglobwb_ulysses_reference_runs_version_2020-12-01/new-jgw_uly-et0_uly-lcv/continue_from_2001/global/states/",DATE_FOR_INITIAL_STATES="2010-12-31" job_script_pbs_pcrglobwb_template.sh)
+
+set +x
