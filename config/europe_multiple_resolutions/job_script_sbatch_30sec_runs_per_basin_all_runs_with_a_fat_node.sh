fd3d3910365ea4f3334459d50f569d29198e2d0c
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-06-22 23:14:49 +0200

    start all runs

--- config/europe_multiple_resolutions/job_script_sbatch_30sec_runs_per_basin_all_runs_with_a_fat_node.sh
+++ config/europe_multiple_resolutions/job_script_sbatch_30sec_runs_per_basin_all_runs_with_a_fat_node.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
 #SBATCH -N 1
-#SBATCH -t 119:59:00
-#SBATCH -p normal
+#~ #SBATCH -t 119:59:00
+#~ #SBATCH -p normal
 #~ #SBATCH --constraint=haswell
 
 #~ #SBATCH -t 59:00
 #~ #SBATCH -p short
 
-#~ #SBATCH -t 119:59:00
-#~ #SBATCH -p fat
+#SBATCH -t 119:59:00
+#SBATCH -p fat
 
 #SBATCH -J eu1k_all
 
