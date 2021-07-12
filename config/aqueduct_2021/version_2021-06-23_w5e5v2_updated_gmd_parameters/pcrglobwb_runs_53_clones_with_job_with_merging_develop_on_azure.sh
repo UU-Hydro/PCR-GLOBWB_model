1511706f19a43f4067b8364747d44f675f78dc79
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-07-12 23:42:41 +0200

    test version_2021-06-23_w5e5v2_updated_gmd_parameters on azure - with a node with 256 ram, but 32 cores - with 25  year spinup

--- config/aqueduct_2021/version_2021-06-23_w5e5v2_updated_gmd_parameters/pcrglobwb_runs_53_clones_with_job_with_merging_develop_on_azure.sh
+++ config/aqueduct_2021/version_2021-06-23_w5e5v2_updated_gmd_parameters/pcrglobwb_runs_53_clones_with_job_with_merging_develop_on_azure.sh
@@ -54,8 +54,10 @@ MAIN_OUTPUT_DIR="/scratch/depfg/sutan101/pcrglobwb_aqueduct_2021/version_2021-06
 # on azure
 MAIN_OUTPUT_DIR="/datadrive/pcrglobwb/pcrglobwb_aqueduct_2021/version_2021-06-23_w5e5v2_updated_gmd_parameters/"
 
+
 STARTING_DATE="1978-01-01"
 END_DATE="2019-12-31"
+
 
 PRECIPITATION_FORCING_FILE="/scratch/depfg/sutan101/data/isimip_forcing/w5e5_version_2.0/downloaded_on_2021-06-09/merged/merged_1979-2019_with_climatology_on_1978/pr_W5E5v2.0_19790101-20191231_with_climatology_on_1978.nc"
 TEMPERATURE_FORCING_FILE="/scratch/depfg/sutan101/data/isimip_forcing/w5e5_version_2.0/downloaded_on_2021-06-09/merged/merged_1979-2019_with_climatology_on_1978/tas_W5E5v2.0_19790101-20191231_with_climatology_on_1978.nc"
@@ -97,11 +99,11 @@ PCRGLOBWB_MODEL_SCRIPT_FOLDER="/home/pcrglobwb/github/edwinkost/PCR-GLOBWB_model
 
 
 
-# load modules on eejit
-. /quanta1/home/sutan101/load_my_miniconda_and_my_default_env.sh
-
 #~ # load modules on eejit
-#~ source activate pcrglobwb_python3
+#~ . /quanta1/home/sutan101/load_my_miniconda_and_my_default_env.sh
+
+# load modules on azure
+source activate pcrglobwb_python3
 
 
 pcrcalc
