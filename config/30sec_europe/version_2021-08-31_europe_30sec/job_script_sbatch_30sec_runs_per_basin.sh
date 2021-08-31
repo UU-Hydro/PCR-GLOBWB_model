c5821876ec415e0bf55c58e212653fc859b09d83
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-08-31 13:15:10 +0200

    start the entire europe run for Jannis

--- config/30sec_europe/version_2021-08-31_europe_30sec/job_script_sbatch_30sec_runs_per_basin.sh
+++ config/30sec_europe/version_2021-08-31_europe_30sec/job_script_sbatch_30sec_runs_per_basin.sh
@@ -1,13 +1,14 @@
 #!/bin/bash 
 
 #SBATCH -N 1
-#SBATCH -t 119:59:00
+#~ #SBATCH -t 119:59:00
+
 #~ #SBATCH -p broadwell
 
 #SBATCH -p normal
 
-#~ #SBATCH -t 59:00
-#~ #SBATCH -p short
+#SBATCH -t 59:00
+#SBATCH -p short
 
 #~ #SBATCH -t 119:59:00
 #~ #SBATCH -p fat
