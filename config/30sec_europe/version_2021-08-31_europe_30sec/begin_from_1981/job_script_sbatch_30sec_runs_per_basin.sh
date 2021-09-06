c744b7c0f035bb05e0148e042b945cda3b182a3f
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-09-06 19:55:22 +0200

    continue from model runs, begin from 1981

--- config/30sec_europe/version_2021-08-31_europe_30sec/begin_from_1981/job_script_sbatch_30sec_runs_per_basin.sh
+++ config/30sec_europe/version_2021-08-31_europe_30sec/begin_from_1981/job_script_sbatch_30sec_runs_per_basin.sh
@@ -2,13 +2,13 @@
 
 #SBATCH -N 1
 
-#~ #SBATCH -t 119:59:00
-#~ #SBATCH -p normal
+#SBATCH -t 119:59:00
+#SBATCH -p normal
 
 #~ #SBATCH -p broadwell
 
-#SBATCH -t 59:00
-#SBATCH -p short
+#~ #SBATCH -t 59:00
+#~ #SBATCH -p short
 
 #~ #SBATCH -t 119:59:00
 #~ #SBATCH -p fat
