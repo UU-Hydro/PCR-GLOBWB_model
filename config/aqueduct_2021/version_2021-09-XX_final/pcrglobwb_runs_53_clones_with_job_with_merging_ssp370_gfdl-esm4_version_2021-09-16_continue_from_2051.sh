67e1b633dabef2ef14b39533d56c53d7154f9cc3
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-09-16 23:38:45 +0200

    test continue ssp370

--- config/aqueduct_2021/version_2021-09-XX_final/pcrglobwb_runs_53_clones_with_job_with_merging_ssp370_gfdl-esm4_version_2021-09-16_continue_from_2051.sh
+++ config/aqueduct_2021/version_2021-09-XX_final/pcrglobwb_runs_53_clones_with_job_with_merging_ssp370_gfdl-esm4_version_2021-09-16_continue_from_2051.sh
@@ -18,12 +18,12 @@ END_DATE="2100-12-31"
 
 
 # meteorological forcing files
-RELATIVE_HUMIDITY_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_hurs_global_daily_1850_2014.nc"
-PRECIPITATION_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_pr_global_daily_1850_2014.nc"
-PRESSURE_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_ps_global_daily_1850_2014.nc"
-SHORTWAVE_RADIATION_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_rsds_global_daily_1850_2014.nc"
-WIND_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_sfcwind_global_daily_1850_2014.nc"
-TEMPERATURE_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_tas_global_daily_1850_2014.nc"
+RELATIVE_HUMIDITY_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_hurs_global_daily_2015_2100.nc"
+PRECIPITATION_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_pr_global_daily_2015_2100.nc"
+PRESSURE_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_ps_global_daily_2015_2100.nc"
+SHORTWAVE_RADIATION_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_rsds_global_daily_2015_2100.nc"
+WIND_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_sfcwind_global_daily_2015_2100.nc"
+TEMPERATURE_FORCING_FILE="/mnt/pcrglobwb/pcrglobwb_input/forcing/gfdl-esm4_w5e5_ssp370_tas_global_daily_2015_2100.nc"
 
 
 # initial conditions - example on eejit
