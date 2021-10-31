10cd0e8efdd5533d6dc65ef96ca865678c7d0cab
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-10-31 14:59:37 +0100

    merging daily pet0 - 1979-2014

--- various_tools/merging_netcdf_aqueduct_2021/merging_daily_generic/bash_historical_only_pet0_1979-2014.sh
+++ various_tools/merging_netcdf_aqueduct_2021/merging_daily_generic/bash_historical_only_pet0_1979-2014.sh
@@ -3,23 +3,23 @@ source activate pcrglobwb_python3
 
 GCM_CODE="gfdl-esm4" 
 GCM_CODE_SHORT="gfdl"
-python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
+python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
 
 GCM_CODE="ipsl-cm6a-lr"
 GCM_CODE_SHORT="ipsl"
-python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
+python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
 
 GCM_CODE="mpi-esm1-2-hr"
 GCM_CODE_SHORT="mpi"
-python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
+python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
 
 GCM_CODE="mri-esm2-0"
 GCM_CODE_SHORT="mri"
-python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
+python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
 
 GCM_CODE="ukesm1-0-ll"
 GCM_CODE_SHORT="ukesm"
-python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
+python merging_daily_variables_only_et0.py /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/ 1979 2014 /ssp_disks/links_to_ssps_all/${GCM_CODE_SHORT}/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/${GCM_CODE}/historical/begin_from_1960/global/netcdf_daily_1979-2014/ &
 
 wait
 
--- various_tools/merging_netcdf_aqueduct_2021/merging_daily_generic/merging_daily_variables_only_et0.py
+++ various_tools/merging_netcdf_aqueduct_2021/merging_daily_generic/merging_daily_variables_only_et0.py
@@ -37,9 +37,10 @@ cmd = ""
 
 for i_year in range(0, len(start_years)):
     
-    # - input sub folder
-    inp_sub_folder = "begin_from_" + str(start_years[i_year]) 
-    if i_year > 0: inp_sub_folder = "continue_from_" + str(start_years[i_year])
+    # ~ # - input sub folder
+    # ~ inp_sub_folder = "begin_from_" + str(start_years[i_year]) 
+    # ~ if i_year > 0: inp_sub_folder = "continue_from_" + str(start_years[i_year])
+    inp_sub_folder = ""
     
     input_folder = main_folder + "/" + inp_sub_folder + "/"
     
