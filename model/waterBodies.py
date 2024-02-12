4319743f92b7de48c7accef991423a738fca94c2
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-02-12 16:28:17 +0100

    test lue

--- model/waterBodies.py
+++ model/waterBodies.py
@@ -416,17 +416,17 @@ class WaterBodies(object):
              length_of_time_step,\
              downstreamDemand)
         
-        if self.debugWaterBalance:\
-           vos.waterBalanceCheck([          pcr.cover(self.inflow/self.waterBodyArea,0.0)],\
-                                 [pcr.cover(self.waterBodyOutflow/self.waterBodyArea,0.0)],\
-                                 [           pcr.cover(preStorage/self.waterBodyArea,0.0)],\
-                                 [pcr.cover(self.waterBodyStorage/self.waterBodyArea,0.0)],\
-                                   'WaterBodyStorage (unit: m)',\
-                                  True,\
-                                  currTimeStep.fulldate,threshold=5e-3)
-        
-           self.waterBodyBalance = (pcr.cover(self.inflow/self.waterBodyArea, 0.0) - pcr.cover(self.waterBodyOutflow/self.waterBodyArea,0.0)) -\
-                                   (pcr.cover(self.waterBodyStorage/self.waterBodyArea,0.0) - pcr.cover(preStorage/self.waterBodyArea,0.0))
+        if self.debugWaterBalance:
+            vos.waterBalanceCheck([          pcr.cover(self.inflow/self.waterBodyArea,0.0)],\
+                                  [pcr.cover(self.waterBodyOutflow/self.waterBodyArea,0.0)],\
+                                  [           pcr.cover(preStorage/self.waterBodyArea,0.0)],\
+                                  [pcr.cover(self.waterBodyStorage/self.waterBodyArea,0.0)],\
+                                    'WaterBodyStorage (unit: m)',\
+                                   True,\
+                                   currTimeStep.fulldate,threshold=5e-3)
+		    
+            self.waterBodyBalance = (pcr.cover(self.inflow/self.waterBodyArea, 0.0) - pcr.cover(self.waterBodyOutflow/self.waterBodyArea,0.0)) -\
+                                    (pcr.cover(self.waterBodyStorage/self.waterBodyArea,0.0) - pcr.cover(preStorage/self.waterBodyArea,0.0))
                                   
 
     def moveFromChannelToWaterBody(self,\
