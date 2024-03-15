df301792fa79e3c80ef8fc7acf8b4931f6df5968
Edwin Sutanudjaja <edwinkost@UU105378>
2024-03-15 12:31:07 +0100

    merging some non-naturalized aqueduct files

--- example_merging/merge_netcdf_example.sh
+++ example_merging/merge_netcdf_example.sh
@@ -16,7 +16,7 @@
 . /eejit/home/sutan101/load_anaconda_and_my_default_env.sh
 
 # go to the folder where the python script is located
-cd /eejit/home/sutan101/github/UU-Hydro/PCR-GLOBWB_model/
+cd /eejit/home/sutan101/github/UU-Hydro/PCR-GLOBWB_model/model/
 
 # merging monthly total files
 python merge_netcdf_general.py /scratch/depfg/sutan101/pcrglobwb_aqueduct_2021_naturalized/version_2021-09-16_naturalized/gswp3-w5e5/historical-reference/continue_from_1965/ /scratch/depfg/sutan101/pcrglobwb_aqueduct_2021_naturalized/version_2021-09-16_naturalized/gswp3-w5e5/historical-reference/continue_from_1965/global/netcdf_1979-2019/ outMonthTotNC 1979-01-31 2019-12-31 referencePotET,gwRecharge,directRunoff,interflowTotal,baseflow,storGroundwater NETCDF4 True 8 53 all_lats &
