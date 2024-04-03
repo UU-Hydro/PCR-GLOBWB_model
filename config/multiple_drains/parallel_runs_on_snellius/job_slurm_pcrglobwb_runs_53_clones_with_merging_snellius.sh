d9cecbfbc83fc8d069f365d9f02dcc67b5a25bd0
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-04-03 12:38:16 +0200

    create an example for  parallel runs for snellius

--- config/multiple_drains/parallel_runs_on_snellius/job_slurm_pcrglobwb_runs_53_clones_with_merging_snellius.sh
+++ config/multiple_drains/parallel_runs_on_snellius/job_slurm_pcrglobwb_runs_53_clones_with_merging_snellius.sh
@@ -1,56 +1,77 @@
-#!/bin/bash 
+#!/bin/bash
+
+# using the genoa computational node
+#SBATCH -p genoa
+
+# we will use 96 cores within one node
+#SBATCH -N 1
+## you reserve one node
+#SBATCH -n 96
+## 96 cores
+
+# wall clock time, 
+#SBATCH -t 119:59:00
+## this is the time, maximum 120:00:00 hours
+
+# job name
+#SBATCH -J pgb_test
+## this is the job name
+
+
+#~ # mail alert at start, end and abortion of execution
+#~ #SBATCH --mail-type=ALL
+#~
+#~ # send mail to this address
+#~ #SBATCH --mail-user=XXX@gmail.com
+
 
 
 set -x
 
 
-# folder containing .ini file
-# - with bash
-INI_FOLDER=$(pwd)
-
-
 # configuration (.ini) file
-# - historical
-INI_FILE=${INI_FOLDER}/"setup_05min_historical_version_2021-09-16.ini"
-#~ # - ssp370
-#~ INI_FILE=${INI_FOLDER}/"setup_05min_ssp370_version_2021-09-16.ini"
+INI_FILE="/home/edwindql/github/UU-Hydro/PCR-GLOBWB_model/config/multiple_drains/parallel_runs_on_snellius/setup_05min_non-linear-gw_based-on_aqueduct-2021.ini"
 
 
 # starting and end dates
 # - historical
-STARTING_DATE="1960-01-01"
-END_DATE="2014-12-31"
+STARTING_DATE="1978-01-01"
+END_DATE="2019-12-31"
 # - PS: for continuing runs (including the transition from the historical to SSP runs), plese use the output files from previous model runs.
 
 
 # location/folder, where you will store output files of your 
 # - historical
-MAIN_OUTPUT_DIR="/datadrive/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/gfdl-esm4/historical/begin_from_1960/"
-#~ # - ssp370
-#~ MAIN_OUTPUT_DIR="/datadrive/pcrglobwb/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/gfdl-esm4/ssp370/continue_from_2015/"
+MAIN_OUTPUT_DIR="/scrcatch-shared/edwindql/pcrglobwb_test_parallel/"
 
 
 # meteorological forcing files
 
-# - historical reference - gswp3-w5e5
-RELATIVE_HUMIDITY_FORCING_FILE="/mnt/pcrglobwb/forcing/gfdl-esm4_w5e5_historical_hurs_global_daily_1850_2014.nc"
-PRECIPITATION_FORCING_FILE="/mnt/pcrglobwb/forcing/gfdl-esm4_w5e5_historical_pr_global_daily_1850_2014.nc"
-PRESSURE_FORCING_FILE="/mnt/pcrglobwb/forcing/gfdl-esm4_w5e5_historical_ps_global_daily_1850_2014.nc"
-SHORTWAVE_RADIATION_FORCING_FILE="/mnt/pcrglobwb/forcing/gfdl-esm4_w5e5_historical_rsds_global_daily_1850_2014.nc"
-WIND_FORCING_FILE="/mnt/pcrglobwb/forcing/gfdl-esm4_w5e5_historical_sfcwind_global_daily_1850_2014.nc"
-TEMPERATURE_FORCING_FILE="/mnt/pcrglobwb/forcing/gfdl-esm4_w5e5_historical_tas_global_daily_1850_2014.nc"
+#~ # - historical reference - gswp3-w5e5
+#~ edwin@int5:/projects/0/dfguu/users/edwin/data/isimip_forcing/isimip3a_version_2021-09-XX/copied_on_2021-09-XX/GSWP3-W5E5/merged/w5e5_1979-2019_with_climatology_on_1978$
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_hurs_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_huss_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_pr_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_prsn_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_ps_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_rlds_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_rsds_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_sfcwind_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_tas_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_tasmax_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
+#~ -rw-r--r-- 1 edwin dfguu  15G Apr 18  2022 gswp3-w5e5_obsclim_tasmin_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc
 
-#~ sutan101@gpu038.cluster:/mnt/pcrglobwb/forcing$ ls -lah *
-#~ -r--r--r-- 1 sutan101 depfg 59G Aug  3 15:45 gfdl-esm4_w5e5_historical_hurs_global_daily_1850_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 59G Aug  3 15:47 gfdl-esm4_w5e5_historical_pr_global_daily_1850_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 59G Aug  3 15:45 gfdl-esm4_w5e5_historical_ps_global_daily_1850_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 59G Aug  3 15:43 gfdl-esm4_w5e5_historical_rsds_global_daily_1850_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 59G Aug  3 15:45 gfdl-esm4_w5e5_historical_sfcwind_global_daily_1850_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 59G Aug  3 15:44 gfdl-esm4_w5e5_historical_tas_global_daily_1850_2014.nc
+
+RELATIVE_HUMIDITY_FORCING_FILE="/projects/0/dfguu/users/edwin/data/isimip_forcing/isimip3a_version_2021-09-XX/copied_on_2021-09-XX/GSWP3-W5E5/merged/w5e5_1979-2019_with_climatology_on_1978/gswp3-w5e5_obsclim_hurs_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc"
+PRECIPITATION_FORCING_FILE="/projects/0/dfguu/users/edwin/data/isimip_forcing/isimip3a_version_2021-09-XX/copied_on_2021-09-XX/GSWP3-W5E5/merged/w5e5_1979-2019_with_climatology_on_1978/gswp3-w5e5_obsclim_pr_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc"
+PRESSURE_FORCING_FILE="/projects/0/dfguu/users/edwin/data/isimip_forcing/isimip3a_version_2021-09-XX/copied_on_2021-09-XX/GSWP3-W5E5/merged/w5e5_1979-2019_with_climatology_on_1978/gswp3-w5e5_obsclim_ps_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc"
+SHORTWAVE_RADIATION_FORCING_FILE="/projects/0/dfguu/users/edwin/data/isimip_forcing/isimip3a_version_2021-09-XX/copied_on_2021-09-XX/GSWP3-W5E5/merged/w5e5_1979-2019_with_climatology_on_1978/gswp3-w5e5_obsclim_rsds_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc"
+WIND_FORCING_FILE="/projects/0/dfguu/users/edwin/data/isimip_forcing/isimip3a_version_2021-09-XX/copied_on_2021-09-XX/GSWP3-W5E5/merged/w5e5_1979-2019_with_climatology_on_1978/gswp3-w5e5_obsclim_rsds_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc"
+TEMPERATURE_FORCING_FILE="/projects/0/dfguu/users/edwin/data/isimip_forcing/isimip3a_version_2021-09-XX/copied_on_2021-09-XX/GSWP3-W5E5/merged/w5e5_1979-2019_with_climatology_on_1978/gswp3-w5e5_obsclim_tas_global_daily_1979-2019_version_2021-09-XX_with_climatology_on_1978.nc"
 
 
 # initial conditions
-MAIN_INITIAL_STATE_FOLDER="/mnt/pcrglobwb/pcrglobwb_input/initial_conditions/"
+MAIN_INITIAL_STATE_FOLDER="/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/initial_conditions/"
 DATE_FOR_INITIAL_STATES="2019-12-31"
 # - PS: for continuing runs (including the transition from the historical to SSP runs), plese use the output files from the previous period model runs.
 
@@ -65,8 +86,9 @@ NUMBER_OF_SPINUP_YEARS="25"
 PCRGLOBWB_MODEL_SCRIPT_FOLDER=~/PCR-GLOBWB_model/model/
 
 
-# load the conda enviroment on azure
-source activate pcrglobwb_python3
+# load the conda enviroment 
+# - using the one from /home/edwin/
+. /home/edwin/load_all_default.sh
 
 
 # unset pcraster working threads (due to a limited number of cores on the Azure VM)
