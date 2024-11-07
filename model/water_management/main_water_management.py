8e0872a2d1ead3b8812553b9bb728c217148485d
Edwin Sutanudjaja <edwinkost@UU105378>
2024-11-07 15:30:21 +0100

    debug and fix

--- model/water_management/main_water_management.py
+++ model/water_management/main_water_management.py
@@ -442,6 +442,9 @@ class WaterManagement(object):
              self.remaining_gross_sectoral_water_demands[sector_name] -= self.allocated_demand_per_sector["desalinated_water"][sector_name]
              self.remaining_gross_sectoral_water_demands[sector_name]  = pcr.max(0.0, self.remaining_gross_sectoral_water_demands[sector_name])
         
+        os.system("killall aguila")
+        vos.aguila_with_var_name(self.satisfied_gross_sectoral_water_demands["irrigation"], "satisfied_irrigation_demand_volume.map")
+        pietje
         
         # abstract and allocate surface water
         # - this will return the following:
@@ -482,9 +485,9 @@ class WaterManagement(object):
              self.remaining_gross_sectoral_water_demands[sector_name]  = pcr.max(0.0, self.remaining_gross_sectoral_water_demands[sector_name])
 
 
-        os.system("killall aguila")
-        vos.aguila_with_var_name(self.satisfied_gross_sectoral_water_demands["irrigation"], "satisfied_irrigation_demand_volume.map")
-        pietje
+        # ~ os.system("killall aguila")
+        # ~ vos.aguila_with_var_name(self.satisfied_gross_sectoral_water_demands["irrigation"], "satisfied_irrigation_demand_volume.map")
+        # ~ pietje
 
         # update the following after abstraction and allocation of nonrenewable_groundwater
         #   - the updated self.remaining_gross_sectoral_water_demands
