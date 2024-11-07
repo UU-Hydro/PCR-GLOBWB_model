3ccdd98e8a41d42c1dd1ef93bf573689c26f784f
Edwin Sutanudjaja <edwinkost@UU105378>
2024-11-07 15:13:34 +0100

    debug and fix

--- model/landSurface.py
+++ model/landSurface.py
@@ -1352,7 +1352,7 @@ class LandSurface(object):
                 # - in water slice/height (m)
                 self.satisfied_irrigation_water_height[coverType] = pcr.ifthenelse(self.landCoverObj[coverType].fracVegCover > 0.0, self.satisfied_irrigation_water_volume[coverType] / (routing.cellArea * self.landCoverObj[coverType].fracVegCover), pcr.scalar(0.0))
 
-                # ~ pcr.aguila(self.satisfied_irrigation_water_height[coverType])
+                pcr.aguila(self.satisfied_irrigation_water_height[coverType])
                 
                 # ~ self.satisfied_irrigation_water_volume[coverType] = pcr.cover(self.satisfied_irrigation_water_volume[coverType], 0.0)
                 # ~ self.satisfied_irrigation_water_height[coverType] = pcr.cover(self.satisfied_irrigation_water_height[coverType], 0.0)
