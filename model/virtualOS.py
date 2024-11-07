1203afd2af45ffac289fd0605969ace903398c64
Edwin Sutanudjaja <edwinkost@UU105378>
2024-11-07 13:14:49 +0100

    add a function to visualize/aguila with file name

--- model/virtualOS.py
+++ model/virtualOS.py
@@ -76,7 +76,7 @@ def aguila_with_var_name(pcr_field, file_name, tmp_directory = None):
     
     # save the variable to a pcraster file and visualize it using aguila
     pcr.report(pcr_field, file_name)
-    cmd = "aguila " + str(file_name) + " & sleep 0.5s & wait"
+    cmd = "aguila " + str(file_name) + " & sleep 0.5s &"
     print(cmd)
     os.system(cmd)
     
