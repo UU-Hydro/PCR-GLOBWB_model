e2edb460eaffc98e04e1e8e9fa20f4b2cc00acb6
Edwin Sutanudjaja <edwinkost@UU105378>
2024-11-07 15:44:24 +0100

    debug and fix

--- model/water_management/main_water_management.py
+++ model/water_management/main_water_management.py
@@ -456,6 +456,10 @@ class WaterManagement(object):
                                                            groundwater                            = groundwater,\
                                                            currTimeStep                           = currTimeStep)
 
+        os.system("killall aguila")
+        vos.aguila_with_var_name(self.satisfied_gross_sectoral_water_demands["irrigation"], "satisfied_irrigation_demand_volume.map")
+        pietje
+
         # update the following after abstraction and allocation of surface water
         #   - the updated self.remaining_gross_sectoral_water_demands
         #   - the updated self.satisfied_gross_sectoral_water_demands
@@ -464,9 +468,9 @@ class WaterManagement(object):
              self.remaining_gross_sectoral_water_demands[sector_name] -= self.allocated_demand_per_sector["surface_water"][sector_name]
              self.remaining_gross_sectoral_water_demands[sector_name]  = pcr.max(0.0, self.remaining_gross_sectoral_water_demands[sector_name])
              
-        os.system("killall aguila")
-        vos.aguila_with_var_name(self.satisfied_gross_sectoral_water_demands["irrigation"], "satisfied_irrigation_demand_volume.map")
-        pietje
+        # ~ os.system("killall aguila")
+        # ~ vos.aguila_with_var_name(self.satisfied_gross_sectoral_water_demands["irrigation"], "satisfied_irrigation_demand_volume.map")
+        # ~ pietje
 
         # abstract and allocate groundwater
         # - this will return the following:
