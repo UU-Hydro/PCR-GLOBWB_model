7b16a74a8fc5d88af13e0be9da600ba71914b628
Edwin Sutanudjaja <edwinkost@UU105378>
2024-11-20 12:56:27 +0100

    test with lue

--- model/pcrglobwb.py
+++ model/pcrglobwb.py
@@ -37,6 +37,15 @@ import groundwater
 import routing
 
 
+def setclone(pathname):
+    pcr.configuration.bounding_box = pcr.BoundingBox(north=90, west=-180, south=-90, east=180)
+    pcr.configuration.cell_size = 0.5
+    pcr.configuration.array_shape = (360, 720)
+    pcr.configuration.partition_shape = (360, 720)
+    
+pcr.setclone = setclone
+
+
 import logging
 logger = logging.getLogger(__name__)
 
