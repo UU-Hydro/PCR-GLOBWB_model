7bfb8cca895dcd6810027faa51cd612e3add935c
Edwin Sutanudjaja <edwinkost@UU105378>
2025-01-17 12:27:16 +0100

    add config file(s) for pm testing

--- model/meteo.py
+++ model/meteo.py
@@ -672,7 +672,7 @@ class Meteo(object):
             outgoing_net_longwave_radiation  = self.longwave_radiation
             
             # calculate net radiation (unit: W.m**-2)
-            self.net_radiation = pcr.max(0.0, incoming_net_shortwave radiation - outgoing_net_longwave_radiation)
+            self.net_radiation = pcr.max(0.0, incoming_net_shortwave_radiation - outgoing_net_longwave_radiation)
 
             # referencePotET in m.day-1
             self.referencePotET = self.penman_monteith.updatePotentialEvaporation(netRadiation        = self.net_radiation, 
