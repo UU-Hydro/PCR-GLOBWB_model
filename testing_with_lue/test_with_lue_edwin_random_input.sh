3be3668cfc32c8481579d64b66602cfb0a655fc0
Edwin Sutanudjaja <edwinkost@UU105378>
2025-03-17 14:15:05 +0100

    keep working

--- testing_with_lue/test_with_lue_edwin_random_input.sh
+++ testing_with_lue/test_with_lue_edwin_random_input.sh
@@ -41,7 +41,7 @@ libtcmalloc=$(find $EBROOTGPERFTOOLS -name libtcmalloc_minimal.so.4)
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
 LUE_PARTITION_SHAPE="360,720" \
-     python /home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/sutan101/github/edwinkost/PCR-GLOBWB_model/config/lue/setup_30min_on_velocity_for_lue_random_field_develop.ini debug \
+     python /home/sutan101/github/edwinkost/PCR-GLOBWB_model/model/deterministic_runner.py /home/sutan101/github/edwinkost/PCR-GLOBWB_model/config/lue/setup_30min_on_velocity_for_lue_random_field_develop.ini nodebug \
          --hpx:threads=1 \
          --lue:dummy1=0 \
          --lue:dummy2=-1 \
