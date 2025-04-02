eceb3f2f76c4b00cdca74bafa4c611dc61b553f1
Edwin Sutanudjaja <edwinkost@UU105378>
2025-04-02 10:51:08 +0200

    another test with lue on eejit

--- testing_with_lue/test_with_lue_edwin_random_input_eejit.sh
+++ testing_with_lue/test_with_lue_edwin_random_input_eejit.sh
@@ -42,11 +42,11 @@ pcrglobwbdebugmode="debug"
 
 # prepare the clone map
 rm /scratch/depfg/sutan101/clone_map_for_lue/clone_map_for_lue_test.map
-mapattr -s -P yb2t -R 10000 -C 20000 -B -x -18 -y 38 -l 0.000833333 /scratch/depfg/sutan101/clone_map_for_lue/clone_map_for_lue_test.map
+mapattr -s -P yb2t -R 1000 -C 2000 -B -x -18 -y 38 -l 0.000833333 /scratch/depfg/sutan101/clone_map_for_lue/clone_map_for_lue_test.map
 
 LD_PRELOAD=$libtcmalloc \
 LUE_PCRASTER_PROVIDER_NAME=lue \
-LUE_PARTITION_SHAPE="5000,10000" \
+LUE_PARTITION_SHAPE="1000,2000" \
      python ${pcrglobwbrunner} ${pcrglobwbini} ${pcrglobwbdebugmode} \
          --hpx:threads=8 \
          --lue:dummy1=0 \
