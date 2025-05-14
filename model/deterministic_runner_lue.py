0eb0fdaccacc4fce6e2f21751f7d3a65b612c6e2
Edwin Sutanudjaja <edwinkost@UU105378>
2025-05-14 12:35:58 +0200

    Use only one setclone for LUE

--- model/deterministic_runner_lue.py
+++ model/deterministic_runner_lue.py
@@ -94,7 +94,7 @@ class DeterministicRunner(pcrfw.DynamicModel):
         # update model (will pick up current model time from model time object)
         
         self.model.read_forcings()
-        state = self.model.update(report_water_balance=True)
+        state = self.model.update(report_water_balance = False)
         
 
         # ~ #do any needed reporting for this time step        
