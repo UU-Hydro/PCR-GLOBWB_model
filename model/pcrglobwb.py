a7217d60b9a7676b650e31cb24217e277f1e6848
Edwin Sutanudjaja <edwinkost@UU105378>
2025-05-20 10:30:12 +0200

    cleaning up and test again

--- model/pcrglobwb.py
+++ model/pcrglobwb.py
@@ -37,13 +37,13 @@ import groundwater
 import routing
 
 
-def setclone(pathname):
-    pcr.configuration.bounding_box = pcr.BoundingBox(north=90, west=-180, south=-90, east=180)
-    pcr.configuration.cell_size = 0.5
-    pcr.configuration.array_shape = (360, 720)
-    pcr.configuration.partition_shape = (360, 720)
+# ~ def setclone(pathname):
+    # ~ pcr.configuration.bounding_box = pcr.BoundingBox(north=90, west=-180, south=-90, east=180)
+    # ~ pcr.configuration.cell_size = 0.5
+    # ~ pcr.configuration.array_shape = (360, 720)
+    # ~ pcr.configuration.partition_shape = (360, 720)
     
-pcr.setclone = setclone
+# ~ pcr.setclone = setclone
 
 
 import logging
--- model/setclone.py
+++
@@ -1,8 +0,0 @@
-
-
-
-def setclone(pathname):
-    pcr.configuration.bounding_box = pcr.BoundingBox(north=90, west=-180, south=-90, east=180)
-    pcr.configuration.cell_size = 0.5
-    pcr.configuration.array_shape = (360, 720)
-    pcr.configuration.partition_shape = (360, 720)
