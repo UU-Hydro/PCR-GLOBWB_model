ea804f7547c93a1f52d9ccaa22ffb793d2f1059c
Gabriel Cardenas Belleza <carde003@velocity.geo.uu.nl>
2025-06-06 11:48:05 +0200

    updating configuration file for parallelization

--- model/water_management_qualloc/config/qualloc_runner_slurm.sh
+++ model/water_management_qualloc/config/qualloc_runner_slurm.sh
@@ -34,7 +34,7 @@ MAIN_OUTPUT_DIR="/scratch/depfg/carde003/qualloc/outputs/historic/true"
 MASK_DIR="maps/masks/mask_"
 
 # directory where initial states are stored
-INI_DIR="initial/historic/"
+INI_DIR="initial/historic"
 
 # year for the initial states
 YEAR_INITIAL_STATES="1979"
@@ -42,7 +42,7 @@ YEAR_INITIAL_STATES="1979"
 # run QUAlloc
 #python qualloc/model/qualloc_runner.py qualloc/model/configuration_file_test.cfg
 #for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53
-for i in 01 02
+for i in 01 02 03 04 05
   do
   # adjusting general input data
   OUTPUT_DIR=${MAIN_OUTPUT_DIR}/M${i}
