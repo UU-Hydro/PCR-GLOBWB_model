4b05962b41df8dfd430a7f7343eeabc6725a9dbc
Gabriel Cardenas Belleza <carde003@velocity.geo.uu.nl>
2025-06-13 17:03:50 +0200

    updating QUAlloc for coupling

--- model/water_management_qualloc/qualloc_main.py
+++ model/water_management_qualloc/qualloc_main.py
@@ -799,14 +799,9 @@ class qualloc_model(object):
         self.gross_demand_forcing_vars = {}
         self.net_demand_forcing_vars = {}
         
-        print(forcing_variables.keys())
-        
         for forcing_variable in forcing_variables.keys():
             for sector_name in self.water_management.sector_names:
                 for demand_name_root in ['%s_gross_demand', '%sgrossdemand']:
-                    
-                    print(f'{forcing_variable.lower()} -> {demand_name_root % sector_name} ( {forcing_variable.lower() == (demand_name_root % sector_name)} )')
-                    
                     if forcing_variable.lower() == (demand_name_root % sector_name):
                         self.gross_demand_forcing_vars[sector_name] = forcing_variable.lower()
                 
