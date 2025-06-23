7fe3417d9473eaacea324f5db0d6a4827cb2caa1
Gabriel Cardenas Belleza <carde003@velocity.geo.uu.nl>
2025-06-24 00:02:15 +0200

    updating QUAlloc for coupling

--- model/water_management_qualloc/qualloc_main.py
+++ model/water_management_qualloc/qualloc_main.py
@@ -1146,7 +1146,7 @@ class qualloc_model(object):
         
         pcr.aguila(self.water_management.water_quality.constituent_shortterm_quality['surfacewater']['organic'])
         pcr.report(self.water_management.water_quality.constituent_shortterm_quality['surfacewater']['organic'], \
-                   f'/scratch-shared/gcardenas/organic_stm_qualloc_{date[:10]}.map')
+                   f'/scratch-shared/gcardenas/organic_stm_qualloc_{str(date)[:10]}.map')
         
         # [ forcing: water management features ] .........................................................
         #
