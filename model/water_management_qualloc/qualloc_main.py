75d100d76d556a7caf045b044d069e47a856be82
Gabriel Cardenas Belleza <carde003@velocity.geo.uu.nl>
2025-08-07 12:17:50 +0200

    updating QUAlloc for coupling

--- model/water_management_qualloc/qualloc_main.py
+++ model/water_management_qualloc/qualloc_main.py
@@ -1146,7 +1146,7 @@ class qualloc_model(object):
         
         
         
-        if dat.month == 1:
+        if date.month == 1:
             pcr.report(self.water_management.water_quality.constituent_shortterm_quality['surfacewater']['temperature'], f"/scratch-shared/gcardenas/waterTemperature_{str(date)[:10].map}")
         
         
